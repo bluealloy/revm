58f9fe58973ab5531e1ddbe8d9f97e9e9c692ab6
rakita <rakita@users.noreply.github.com>
2021-10-21 17:33:31 +0200

    Modexp istanbul does not have min gas value

--- src/precompiles/modexp.rs
+++ src/precompiles/modexp.rs
@@ -46,14 +46,28 @@ macro_rules! read_u64_with_overflow {
 }
 
 impl<HF: HardFork> ModExp<HF> {
+    fn calculate_iteration_count(exp_length: u64, exp_highp: &BigUint) -> u64 {
+        let mut iteration_count: u64 = 0;
+
+        if exp_length <= 32 && exp_highp.is_zero() {
+            iteration_count = 0;
+        } else if exp_length <= 32 {
+            iteration_count = exp_highp.bits() - 1;
+        } else if exp_length > 32 {
+            iteration_count = (8 * (exp_length - 32)) + max(1, exp_highp.bits()) - 1;
+        }
+
+        max(iteration_count, 1)
+    }
+
     fn run_inner<F>(input: &[u8], gas_limit: u64, calc_gas: F) -> PrecompileResult
     where
         F: FnOnce(u64, u64, u64, &BigUint) -> u64,
     {
         let len = input.len();
-        let (base_len, base_overflow) = read_u64_with_overflow!(input, 0, 32, u16::MAX as usize);
-        let (exp_len, exp_overflow) = read_u64_with_overflow!(input, 32, 64, u16::MAX as usize);
-        let (mod_len, mod_overflow) = read_u64_with_overflow!(input, 64, 96, u16::MAX as usize);
+        let (base_len, base_overflow) = read_u64_with_overflow!(input, 0, 32, u32::MAX as usize);
+        let (exp_len, exp_overflow) = read_u64_with_overflow!(input, 32, 64, u32::MAX as usize);
+        let (mod_len, mod_overflow) = read_u64_with_overflow!(input, 64, 96, u32::MAX as usize);
 
         if base_overflow || mod_overflow {
             return Ok(PrecompileOutput::without_logs(u64::MAX, Vec::new()));
@@ -104,8 +118,6 @@ impl<HF: HardFork> ModExp<HF> {
             }
         };
 
-        let gas_cost = max(gas_cost, MIN_GAS_COST);
-
         // write output to given memory, left padded and same length as the modulus.
         let bytes = r.to_bytes_be();
         // always true except in the case of zero-length modulus, which leads to
@@ -138,22 +150,11 @@ impl ModExp<Byzantium> {
         }
     }
 
-    fn calc_iter_count(exp_len: u64, exponent: &BigUint) -> U256 {
-        if exp_len <= 32 && exponent.is_zero() {
-            U256::zero()
-        } else if exp_len <= 32 {
-            U256::from(exponent.bits()) - U256::from(1)
-        } else {
-            // else > 32
-            U256::from(8) * U256::from(exp_len - 32) + U256::from(exponent.bits()) - U256::from(1)
-        }
-    }
-
-    fn byzantium_gas_calc(base_len: u64, exp_len: u64, mod_len: u64, exponent: &BigUint) -> u64 {
+    fn byzantium_gas_calc(base_len: u64, exp_len: u64, mod_len: u64, exp_highp: &BigUint) -> u64 {
         let mul = Self::mul_complexity(core::cmp::max(mod_len, base_len));
-        let iter_count = Self::calc_iter_count(exp_len, exponent);
+        let iter_count = U256::from(Self::calculate_iteration_count(exp_len, exp_highp));
         // mul * iter_count bounded by 2^195 < 2^256 (no overflow)
-        let gas = mul * core::cmp::max(iter_count, U256::one()) / U256::from(20);
+        let gas = (mul * iter_count) / U256::from(20);
 
         if gas.bits() > 64 {
             u64::MAX
@@ -197,23 +198,9 @@ impl ModExp<Berlin> {
             words * words
         }
 
-        fn calculate_iteration_count(exp_length: u64, exp_highp: &BigUint) -> u64 {
-            let mut iteration_count: u64 = 0;
-
-            if exp_length <= 32 && exp_highp.is_zero() {
-                iteration_count = 0;
-            } else if exp_length <= 32 {
-                iteration_count = exp_highp.bits() - 1;
-            } else if exp_length > 32 {
-                iteration_count = (8 * (exp_length - 32)) + max(1, exp_highp.bits()) - 1;
-            }
-
-            max(iteration_count, 1)
-        }
-
         let multiplication_complexity =
             calculate_multiplication_complexity(base_length, mod_length);
-        let iteration_count = calculate_iteration_count(exp_length, exp_highp);
+        let iteration_count = Self::calculate_iteration_count(exp_length, exp_highp);
         let gas = (multiplication_complexity * U256::from(iteration_count)) / 3;
         if gas > U256::from(u64::MAX) {
             return u64::MAX;
