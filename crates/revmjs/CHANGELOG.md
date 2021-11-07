7f380405f7745629b8fa538a9411118640fba854
rakita <dragan0rakita@gmail.com>
2021-11-07 21:37:19 +0100

    [revmjs] Add bn-rs for easier casting from BN.js to primitive_types

--- crates/revmjs/CHANGELOG.md
+++ crates/revmjs/CHANGELOG.md
@@ -1,3 +1,6 @@
+# v0.2.0
+
+* Added bn-rs lib that converts BN.js to primitive-types.
 # v0.1.0
 
 Initial release with usable example of usage.
--- crates/revmjs/src/lib.rs
+++ crates/revmjs/src/lib.rs
@@ -1,6 +1,9 @@
+use core::convert::TryInto;
+
+use bn_rs::BN;
 use bytes::{Bytes, BytesMut};
 use primitive_types::{H160, H256, U256};
-use revm::{AccountInfo, DatabaseCommit, EVM as rEVM, InMemoryDB, SpecId, TransactTo};
+use revm::{AccountInfo, DatabaseCommit, InMemoryDB, SpecId, TransactTo, EVM as rEVM};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -50,10 +53,10 @@ impl EVM {
     }
 
     /****** DATABASE RELATED ********/
-    pub fn insert_account(&mut self, address: &[u8], nonce: u64, balance: &[u8], code: &[u8]) {
-        let address = H160::from_slice(address);
+    pub fn insert_account(&mut self, address: BN, nonce: u64, balance: BN, code: &[u8]) {
+        let address = address.try_into().unwrap();
         let acc_info = AccountInfo::new(
-            U256::from_big_endian(balance),
+            balance.try_into().unwrap(),
             nonce,
             Bytes::copy_from_slice(code),
         );
@@ -63,8 +66,8 @@ impl EVM {
 
     /****** ALL CFG ENV SETTERS ********/
 
-    pub fn cfg_chain_id(&mut self, gas_limit: &[u8]) {
-        self.revm.env.cfg.chain_id = U256::from_big_endian(gas_limit);
+    pub fn cfg_chain_id(&mut self, gas_limit: BN) {
+        self.revm.env.cfg.chain_id = gas_limit.try_into().unwrap();
     }
     pub fn cfg_spec_id(&mut self, spec_id: u8) {
         self.revm.env.cfg.spec_id = SpecId::try_from_u8(spec_id).unwrap_or_else(|| SpecId::LATEST);
@@ -72,48 +75,44 @@ impl EVM {
 
     /****** ALL BLOCK ENV SETTERS ********/
 
-    pub fn block_gas_limit(&mut self, gas_limit: &[u8]) {
-        self.revm.env.block.gas_limit = U256::from_big_endian(gas_limit);
+    pub fn block_gas_limit(&mut self, gas_limit:  BN) {
+        self.revm.env.block.gas_limit = gas_limit.try_into().unwrap();
     }
-    pub fn block_number(&mut self, number: &[u8]) {
-        self.revm.env.block.number = U256::from_big_endian(number);
+    pub fn block_number(&mut self, number:  BN) {
+        self.revm.env.block.number = number.try_into().unwrap();
     }
-    pub fn block_coinbase(&mut self, coinbase: &[u8]) {
-        self.revm.env.block.coinbase = H160::from_slice(coinbase);
+    pub fn block_coinbase(&mut self, coinbase: BN) {
+        self.revm.env.block.coinbase = coinbase.try_into().unwrap();
     }
-    pub fn block_timestamp(&mut self, timestamp: &[u8]) {
-        self.revm.env.block.timestamp = U256::from_big_endian(timestamp);
+    pub fn block_timestamp(&mut self, timestamp: BN) {
+        self.revm.env.block.timestamp = timestamp.try_into().unwrap();
     }
-    pub fn block_difficulty(&mut self, difficulty: &[u8]) {
-        self.revm.env.block.difficulty = U256::from_big_endian(difficulty);
+    pub fn block_difficulty(&mut self, difficulty: BN) {
+        self.revm.env.block.difficulty = difficulty.try_into().unwrap();
     }
-    pub fn block_basefee(&mut self, basefee: &[u8]) {
-        self.revm.env.block.basefee = U256::from_big_endian(basefee);
+    pub fn block_basefee(&mut self, basefee: BN) {
+        self.revm.env.block.basefee = basefee.try_into().unwrap();
     }
-    pub fn block_gas_used(&mut self, gas_used: &[u8]) {
-        self.revm.env.block.gas_used = U256::from_big_endian(gas_used);
+    pub fn block_gas_used(&mut self, gas_used: BN) {
+        self.revm.env.block.gas_used = gas_used.try_into().unwrap();
     }
 
     /****** ALL TX ENV SETTERS ********/
 
-    pub fn tx_caller(&mut self, tx_caller: &[u8]) {
-        self.revm.env.tx.caller = H160::from_slice(tx_caller);
+    pub fn tx_caller(&mut self, tx_caller: BN) {
+        self.revm.env.tx.caller = tx_caller.try_into().unwrap();
     }
     pub fn tx_gas_limit(&mut self, gas_limit: u64) {
         self.revm.env.tx.gas_limit = gas_limit;
     }
-    pub fn tx_gas_price(&mut self, gas_price: &[u8]) {
-        self.revm.env.tx.gas_price = U256::from_big_endian(gas_price);
+    pub fn tx_gas_price(&mut self, gas_price: BN) {
+        self.revm.env.tx.gas_price = gas_price.try_into().unwrap();
     }
-    pub fn tx_gas_priority_fee(&mut self, gas_priority_fee: &[u8]) {
-        self.revm.env.tx.gas_priority_fee = if gas_priority_fee.len() == 0 {
-            None
-        } else {
-            Some(U256::from_big_endian(gas_priority_fee))
-        };
+    pub fn tx_gas_priority_fee(&mut self, gas_priority_fee: Option<BN>) {
+        self.revm.env.tx.gas_priority_fee = gas_priority_fee.map(|v| v.try_into().unwrap());
     }
-    pub fn tx_value(&mut self, value: &[u8]) {
-        self.revm.env.tx.value = U256::from_big_endian(value);
+    pub fn tx_value(&mut self, value: BN) {
+        self.revm.env.tx.value = value.try_into().unwrap();
     }
     pub fn tx_chain_id(&mut self, chain_id: Option<u64>) {
         self.revm.env.tx.chain_id = chain_id;
@@ -127,8 +126,8 @@ impl EVM {
     pub fn tx_transact_to_create(&mut self) {
         self.revm.env.tx.transact_to = TransactTo::create();
     }
-    pub fn tx_transact_to_call(&mut self, to: &[u8]) {
-        self.revm.env.tx.transact_to = TransactTo::Call(H160::from_slice(to));
+    pub fn tx_transact_to_call(&mut self, to: BN) {
+        self.revm.env.tx.transact_to = TransactTo::Call(to.try_into().unwrap());
     }
     pub fn tx_accessed_account(&mut self, account: AccessedAccount) {
         self.revm.env.tx.access_list.push(account.into())
@@ -150,13 +149,13 @@ impl Into<(H160, Vec<H256>)> for AccessedAccount {
 
 #[wasm_bindgen]
 impl AccessedAccount {
-    pub fn new(account: &[u8]) -> Self {
+    pub fn new(account: BN) -> Self {
         Self {
-            account: H160::from_slice(account),
+            account: account.try_into().unwrap(),
             slots: Vec::new(),
         }
     }
-    pub fn slot(&mut self, slot: &[u8]) {
-        self.slots.push(H256::from_slice(slot))
+    pub fn slot(&mut self, slot: BN) {
+        self.slots.push(slot.try_into().unwrap())
     }
 }
