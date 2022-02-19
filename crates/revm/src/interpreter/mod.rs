5d26a6923a783e036f5fa9204ce40c88c80144f6
rakita <dragan0rakita@gmail.com>
2022-02-19 15:03:01 +0100

    [revm] Move Gas, and machine outside

--- crates/revm/src/interpreter/mod.rs
+++ crates/revm/src/interpreter/mod.rs
@@ -4,8 +4,10 @@ mod machine;
 mod contract;
 pub(crate) mod memory;
 mod stack;
+mod gas;
 
 pub use contract::Contract;
 pub use machine::*;
 pub use memory::Memory;
 pub use stack::Stack;
+pub use gas::Gas;
