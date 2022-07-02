8267f678e1791998daa3fbb58762ebd9474d3b07
rakita <rakita@users.noreply.github.com>
2022-07-02 14:33:08 +0200

    bump k256 (#143)

--- crates/revm_precompiles/src/secp256k1.rs
+++ crates/revm_precompiles/src/secp256k1.rs
@@ -25,7 +25,7 @@ mod secp256k1 {
 
     pub fn ecrecover(sig: &[u8; 65], msg: &[u8; 32]) -> Result<Address, Error> {
         let sig = recoverable::Signature::try_from(sig.as_ref())?;
-        let verify_key = sig.recover_verify_key_from_digest_bytes(msg.into())?;
+        let verify_key = sig.recover_verifying_key_from_digest_bytes(msg.into())?;
         let public_key = K256PublicKey::from(&verify_key);
         let public_key = public_key.to_encoded_point(/* compress = */ false);
         let public_key = public_key.as_bytes();
