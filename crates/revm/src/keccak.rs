31579679262e501d3629db965432e855ed2c395d
Remco Bloemen <remco@wicked.ventures>
2022-10-28 22:15:24 -0700

    Cleanup

--- crates/revm/src/keccak.rs
+++ crates/revm/src/keccak.rs
@@ -36,113 +36,112 @@ pub fn keccak_f1600(state: &mut [u64; 25]) {
     unsafe {
         asm!("
             // Read state
-            ld1	{{ v0.1d- v3.1d}}, [x0], #32
-            ld1	{{ v4.1d- v7.1d}}, [x0], #32
-            ld1	{{ v8.1d-v11.1d}}, [x0], #32
-            ld1	{{v12.1d-v15.1d}}, [x0], #32
-            ld1	{{v16.1d-v19.1d}}, [x0], #32
-            ld1	{{v20.1d-v23.1d}}, [x0], #32
-            ld1	{{v24.1d}}, [x0]
+            ld1 {{ v0.1d- v3.1d}}, [x0], #32
+            ld1 {{ v4.1d- v7.1d}}, [x0], #32
+            ld1 {{ v8.1d-v11.1d}}, [x0], #32
+            ld1 {{v12.1d-v15.1d}}, [x0], #32
+            ld1 {{v16.1d-v19.1d}}, [x0], #32
+            ld1 {{v20.1d-v23.1d}}, [x0], #32
+            ld1 {{v24.1d}}, [x0]
             sub    x0, x0, #192
 
             // Loop 24 rounds
-            mov	x8, #24
-        0:  sub	x8, x8, #1
+            mov x8, #24
+        0:  sub x8, x8, #1
 
             // Theta Calculations
-            eor3	v29.16b,  v4.16b,  v9.16b, v14.16b
-            eor3	v26.16b,  v1.16b,  v6.16b, v11.16b
-            eor3	v28.16b,  v3.16b,  v8.16b, v13.16b
-            eor3	v25.16b,  v0.16b,  v5.16b, v10.16b
-            eor3	v27.16b,  v2.16b,  v7.16b, v12.16b
-            eor3	v29.16b, v29.16b, v19.16b, v24.16b
-            eor3	v26.16b, v26.16b, v16.16b, v21.16b
-            eor3	v28.16b, v28.16b, v18.16b, v23.16b
-            eor3	v25.16b, v25.16b, v15.16b, v20.16b
-            eor3	v27.16b, v27.16b, v17.16b, v22.16b
+            eor3 v29.16b,  v4.16b,  v9.16b, v14.16b
+            eor3 v26.16b,  v1.16b,  v6.16b, v11.16b
+            eor3 v28.16b,  v3.16b,  v8.16b, v13.16b
+            eor3 v25.16b,  v0.16b,  v5.16b, v10.16b
+            eor3 v27.16b,  v2.16b,  v7.16b, v12.16b
+            eor3 v29.16b, v29.16b, v19.16b, v24.16b
+            eor3 v26.16b, v26.16b, v16.16b, v21.16b
+            eor3 v28.16b, v28.16b, v18.16b, v23.16b
+            eor3 v25.16b, v25.16b, v15.16b, v20.16b
+            eor3 v27.16b, v27.16b, v17.16b, v22.16b
 
-            rax1	v30.2d, v29.2d, v26.2d
-            rax1	v26.2d, v26.2d, v28.2d
-            rax1	v28.2d, v28.2d, v25.2d
-            rax1	v25.2d, v25.2d, v27.2d
-            rax1	v27.2d, v27.2d, v29.2d
+            rax1 v30.2d, v29.2d, v26.2d
+            rax1 v26.2d, v26.2d, v28.2d
+            rax1 v28.2d, v28.2d, v25.2d
+            rax1 v25.2d, v25.2d, v27.2d
+            rax1 v27.2d, v27.2d, v29.2d
 
             // Rho and Phi
-            eor	 v0.16b,  v0.16b, v30.16b
-            xar	 v29.2d,   v1.2d,  v25.2d, (64 - 1)
-            xar	  v1.2d,   v6.2d,  v25.2d, (64 - 44)
-            xar	  v6.2d,   v9.2d,  v28.2d, (64 - 20)
-            xar	  v9.2d,  v22.2d,  v26.2d, (64 - 61)
-            xar	 v22.2d,  v14.2d,  v28.2d, (64 - 39)
-            xar	 v14.2d,  v20.2d,  v30.2d, (64 - 18)
-            xar	 v31.2d,   v2.2d,  v26.2d, (64 - 62)
-            xar	  v2.2d,  v12.2d,  v26.2d, (64 - 43)
-            xar	 v12.2d,  v13.2d,  v27.2d, (64 - 25)
-            xar	 v13.2d,  v19.2d,  v28.2d, (64 - 8)
-            xar	 v19.2d,  v23.2d,  v27.2d, (64 - 56)
-            xar	 v23.2d,  v15.2d,  v30.2d, (64 - 41)
-            xar	 v15.2d,   v4.2d,  v28.2d, (64 - 27)
-            xar	 v28.2d,  v24.2d,  v28.2d, (64 - 14)
-            xar	 v24.2d,  v21.2d,  v25.2d, (64 - 2)
-            xar	  v8.2d,   v8.2d,  v27.2d, (64 - 55)
-            xar	  v4.2d,  v16.2d,  v25.2d, (64 - 45)
-            xar	 v16.2d,   v5.2d,  v30.2d, (64 - 36)
-            xar	  v5.2d,   v3.2d,  v27.2d, (64 - 28)
-            xar	 v27.2d,  v18.2d,  v27.2d, (64 - 21)
-            xar	  v3.2d,  v17.2d,  v26.2d, (64 - 15)
-            xar	 v25.2d,  v11.2d,  v25.2d, (64 - 10)
-            xar	 v26.2d,   v7.2d,  v26.2d, (64 - 6)
-            xar	 v30.2d,  v10.2d,  v30.2d, (64 - 3)
+            eor  v0.16b,  v0.16b, v30.16b
+            xar  v29.2d,   v1.2d,  v25.2d, (64 - 1)
+            xar   v1.2d,   v6.2d,  v25.2d, (64 - 44)
+            xar   v6.2d,   v9.2d,  v28.2d, (64 - 20)
+            xar   v9.2d,  v22.2d,  v26.2d, (64 - 61)
+            xar  v22.2d,  v14.2d,  v28.2d, (64 - 39)
+            xar  v14.2d,  v20.2d,  v30.2d, (64 - 18)
+            xar  v31.2d,   v2.2d,  v26.2d, (64 - 62)
+            xar   v2.2d,  v12.2d,  v26.2d, (64 - 43)
+            xar  v12.2d,  v13.2d,  v27.2d, (64 - 25)
+            xar  v13.2d,  v19.2d,  v28.2d, (64 - 8)
+            xar  v19.2d,  v23.2d,  v27.2d, (64 - 56)
+            xar  v23.2d,  v15.2d,  v30.2d, (64 - 41)
+            xar  v15.2d,   v4.2d,  v28.2d, (64 - 27)
+            xar  v28.2d,  v24.2d,  v28.2d, (64 - 14)
+            xar  v24.2d,  v21.2d,  v25.2d, (64 - 2)
+            xar   v8.2d,   v8.2d,  v27.2d, (64 - 55)
+            xar   v4.2d,  v16.2d,  v25.2d, (64 - 45)
+            xar  v16.2d,   v5.2d,  v30.2d, (64 - 36)
+            xar   v5.2d,   v3.2d,  v27.2d, (64 - 28)
+            xar  v27.2d,  v18.2d,  v27.2d, (64 - 21)
+            xar   v3.2d,  v17.2d,  v26.2d, (64 - 15)
+            xar  v25.2d,  v11.2d,  v25.2d, (64 - 10)
+            xar  v26.2d,   v7.2d,  v26.2d, (64 - 6)
+            xar  v30.2d,  v10.2d,  v30.2d, (64 - 3)
 
             // Chi
-            bcax	v20.16b, v31.16b, v22.16b,  v8.16b
-            bcax	v21.16b,  v8.16b, v23.16b, v22.16b
-            bcax	v22.16b, v22.16b, v24.16b, v23.16b
-            bcax	v23.16b, v23.16b, v31.16b, v24.16b
-            bcax	v24.16b, v24.16b,  v8.16b, v31.16b
+            bcax v20.16b, v31.16b, v22.16b,  v8.16b
+            bcax v21.16b,  v8.16b, v23.16b, v22.16b
+            bcax v22.16b, v22.16b, v24.16b, v23.16b
+            bcax v23.16b, v23.16b, v31.16b, v24.16b
+            bcax v24.16b, v24.16b,  v8.16b, v31.16b
 
             // Load round constant in now freed v31
-            ld1r    {{v31.2d}}, [x1]
-            add     x1, x1, #8
+            ld1r    {{v31.2d}}, [x1], #8
 
-            bcax	v17.16b, v25.16b, v19.16b,  v3.16b
-            bcax	v18.16b,  v3.16b, v15.16b, v19.16b
-            bcax	v19.16b, v19.16b, v16.16b, v15.16b
-            bcax	v15.16b, v15.16b, v25.16b, v16.16b
-            bcax	v16.16b, v16.16b,  v3.16b, v25.16b
+            bcax v17.16b, v25.16b, v19.16b,  v3.16b
+            bcax v18.16b,  v3.16b, v15.16b, v19.16b
+            bcax v19.16b, v19.16b, v16.16b, v15.16b
+            bcax v15.16b, v15.16b, v25.16b, v16.16b
+            bcax v16.16b, v16.16b,  v3.16b, v25.16b
 
-            bcax	v10.16b, v29.16b, v12.16b, v26.16b
-            bcax	v11.16b, v26.16b, v13.16b, v12.16b
-            bcax	v12.16b, v12.16b, v14.16b, v13.16b
-            bcax	v13.16b, v13.16b, v29.16b, v14.16b
-            bcax	v14.16b, v14.16b, v26.16b, v29.16b
+            bcax v10.16b, v29.16b, v12.16b, v26.16b
+            bcax v11.16b, v26.16b, v13.16b, v12.16b
+            bcax v12.16b, v12.16b, v14.16b, v13.16b
+            bcax v13.16b, v13.16b, v29.16b, v14.16b
+            bcax v14.16b, v14.16b, v26.16b, v29.16b
 
-            bcax	 v7.16b, v30.16b,  v9.16b,  v4.16b
-            bcax	 v8.16b,  v4.16b,  v5.16b,  v9.16b
-            bcax	 v9.16b,  v9.16b,  v6.16b,  v5.16b
-            bcax	 v5.16b,  v5.16b, v30.16b,  v6.16b
-            bcax	 v6.16b,  v6.16b,  v4.16b, v30.16b
+            bcax  v7.16b, v30.16b,  v9.16b,  v4.16b
+            bcax  v8.16b,  v4.16b,  v5.16b,  v9.16b
+            bcax  v9.16b,  v9.16b,  v6.16b,  v5.16b
+            bcax  v5.16b,  v5.16b, v30.16b,  v6.16b
+            bcax  v6.16b,  v6.16b,  v4.16b, v30.16b
 
-            bcax	 v3.16b, v27.16b,  v0.16b, v28.16b
-            bcax	 v4.16b, v28.16b,  v1.16b,  v0.16b
-            bcax	 v0.16b,  v0.16b,  v2.16b,  v1.16b
-            bcax	 v1.16b,  v1.16b, v27.16b,  v2.16b
-            bcax	 v2.16b,  v2.16b, v28.16b, v27.16b
+            bcax  v3.16b, v27.16b,  v0.16b, v28.16b
+            bcax  v4.16b, v28.16b,  v1.16b,  v0.16b
+            bcax  v0.16b,  v0.16b,  v2.16b,  v1.16b
+            bcax  v1.16b,  v1.16b, v27.16b,  v2.16b
+            bcax  v2.16b,  v2.16b, v28.16b, v27.16b
 
             // Iota: add round constant
-            eor	 v0.16b,  v0.16b, v31.16b
+            eor  v0.16b,  v0.16b, v31.16b
 
             // Rounds loop
             cbnz    w8, 0b
 
             // Write state
-            st1	{{ v0.1d- v3.1d}}, [x0], #32
-            st1	{{ v4.1d- v7.1d}}, [x0], #32
-            st1	{{ v8.1d-v11.1d}}, [x0], #32
-            st1	{{v12.1d-v15.1d}}, [x0], #32
-            st1	{{v16.1d-v19.1d}}, [x0], #32
-            st1	{{v20.1d-v23.1d}}, [x0], #32
-            st1	{{v24.1d}}, [x0]
+            st1 {{ v0.1d- v3.1d}}, [x0], #32
+            st1 {{ v4.1d- v7.1d}}, [x0], #32
+            st1 {{ v8.1d-v11.1d}}, [x0], #32
+            st1 {{v12.1d-v15.1d}}, [x0], #32
+            st1 {{v16.1d-v19.1d}}, [x0], #32
+            st1 {{v20.1d-v23.1d}}, [x0], #32
+            st1 {{v24.1d}}, [x0]
         ",
             in("x0") state.as_mut_ptr(),
             in("x1") &RC,
