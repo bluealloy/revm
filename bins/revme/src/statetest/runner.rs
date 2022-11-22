1ad3ec0fb3aafde97667237e0bb6695a9411a97a
rakita <dragan0rakita@gmail.com>
2022-11-22 16:04:50 +0100

    chore: Bump primitive_types. Add statetest spec

--- bins/revme/src/statetest/runner.rs
+++ bins/revme/src/statetest/runner.rs
@@ -180,6 +180,7 @@ pub fn execute_test_suit(path: &Path, elapsed: &Arc<Mutex<Duration>>) -> Result<
                 SpecName::ByzantiumToConstantinopleAt5
                     | SpecName::Constantinople
                     | SpecName::MergeEOF
+                    | SpecName::MergeMeterInitCode
             ) {
                 continue;
             }
