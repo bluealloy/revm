0337f2d30632da02491af99cb7776f4da17d08fb
0xDmtri <0xDmtri@protonmail.com>
2022-12-15 23:23:50 +0000

    Add example for fork + ref_transact impl

---
+++ examples/fork_ref_transact.rs
@@ -0,0 +1,130 @@
+use std::{str::FromStr, sync::Arc};
+
+use anyhow::{Ok, Result};
+use bytes::Bytes;
+use ethers::{
+    abi::parse_abi,
+    prelude::BaseContract,
+    providers::{Http, Middleware, Provider},
+    types::{H160, H256, U256},
+};
+use revm::{
+    db::{CacheDB, EmptyDB, EthersDB},
+    Database, TransactOut, TransactTo, B160, EVM, U256 as rU256,
+};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // create ethers client and wrap it in Arc<M>
+    let client = Provider::<Http>::try_from(
+        "https://mainnet.infura.io/v3/c60b0bb42f8a4c6481ecd229eddaca27",
+    )?;
+    let client = Arc::new(client);
+
+    // ----------------------------------------------------------- //
+    //         Storage slots of a UniV2Pair like contract          //
+    // *********************************************************** //
+    // location[5] = factory: address                              //
+    // location[6] = token0: address                               //
+    // location[7] = token1: address                               //
+    // location[8] = (res0, res1, ts): (uint112, uint112, uint32)  //
+    // location[9] = price0CumulativeLast: uint256                 //
+    // location[10] = price1CumulativeLast: uint256                //
+    // location[11] = kLast: uint256                               //
+    // *********************************************************** //
+
+    // choose index of storage that you would like to transact with
+    let index = 8;
+    let location = H256::from_low_u64_be(index);
+
+    // ETH/USDT pair on Uniswap V2
+    let pool_address = B160::from_str("0x0d4a11d5EEaaC28EC3F61d100daF4d40471f1852")?;
+
+    // load the reserve slot via ethers-rs
+    let reserves_slot = client
+        // first, parse address (B160) into H160 that ethers can understand
+        .get_storage_at(H160::from(pool_address.0), location, None)
+        .await?;
+
+    let mem = U256::from(reserves_slot.as_bytes());
+
+    // bitshitOoor to extract uint112, uint112, uint32 from uint256
+    let ts_raw = (mem >> (2 * 112)).as_u32();
+    let reserve1_raw = ((mem >> 112) & ((U256::from(1u8) << 112) - 1u8)).as_u128();
+    let reserve0_raw = (mem & ((U256::from(1u8) << 112) - 1u8)).as_u128();
+
+    // generate abi for the calldata from the human readable interface
+    let abi = BaseContract::from(
+        parse_abi(&[
+            "function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast)",
+        ])?
+    );
+
+    // encode abi into Bytes
+    let encoded = abi.encode("getReserves", ())?;
+
+    // initialize new EthersDB
+    let mut ethersdb = EthersDB::new(Arc::clone(&client), None).unwrap();
+
+    // query basic properties of an account incl bytecode
+    let acc_info = ethersdb.basic(pool_address).unwrap().unwrap();
+
+    // initialise empty in-memory-db
+    let mut cache_db = CacheDB::new(EmptyDB::default());
+
+    // insert basic account info which was generated via Web3DB with the corresponding address
+    cache_db.insert_account_info(pool_address, acc_info);
+
+    // insert our pre-loaded storage slot to the corresponding contract key (address) in the DB
+    cache_db
+        .insert_account_storage(
+            pool_address,
+            rU256::from(index),
+            rU256::from_be_bytes(reserves_slot.0),
+        )
+        .unwrap();
+
+    // initialise an empty (default) EVM
+    let mut evm = EVM::new();
+
+    // insert pre-built database from above
+    evm.database(cache_db);
+
+    // fill in missing bits of env struc
+    // change that to whatever caller you want to be
+    evm.env.tx.caller = B160::from_str("0x0000000000000000000000000000000000000000")?;
+    // account you want to transact with
+    evm.env.tx.transact_to = TransactTo::Call(pool_address);
+    // calldata formed via abigen
+    evm.env.tx.data = Bytes::from(hex::decode(hex::encode(&encoded))?);
+    // transaction value in wei
+    evm.env.tx.value = rU256::try_from(0)?;
+
+    // execute transaction without writing to the DB
+    let ref_tx = evm.transact_ref();
+    // select ExecutionResult struct
+    let result = ref_tx.0;
+
+    // unpack output call enum into raw bytes
+    let value = match result.out {
+        TransactOut::Call(value) => Some(value),
+        _ => None,
+    };
+
+    // decode bytes to reserves + ts via ethers-rs's abi decode
+    let (reserve0, reserve1, ts): (u128, u128, u32) =
+        abi.decode_output("getReserves", value.unwrap())?;
+
+    // Print emualted getReserves() call output
+    println!("Reserve0: {:#?}", reserve0);
+    println!("Reserve1: {:#?}", reserve1);
+    println!("Timestamp: {:#?}", ts);
+
+    // make sure that the emulator's call is exactly the same as the raw call
+    assert_eq!(reserve0_raw, reserve0);
+    assert_eq!(reserve1_raw, reserve1);
+    assert_eq!(ts_raw, ts);
+
+    // GG if you are here with no errors
+    Ok(())
+}
