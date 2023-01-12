4953fe82976f828be7fbb1959b25f5abc5e1cf09
Enrique Ortiz <evalir@users.noreply.github.com>
2023-01-11 21:27:02 -0400

    fix: add proper not statement

--- crates/revm/src/lib.rs
+++ crates/revm/src/lib.rs
@@ -6,7 +6,7 @@ mod evm_impl;
 mod inspector;
 mod journaled_state;
 
-#[cfg(all(feature = "with-serde", not("serde" )]
+#[cfg(all(feature = "with-serde", not(feature = "serde")))]
 compile_error!("`with-serde` feature has been renamed to `serde`.");
 
 pub(crate) const USE_GAS: bool = !cfg!(feature = "no_gas_measuring");
