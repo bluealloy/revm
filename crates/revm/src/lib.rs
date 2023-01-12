61d0b49d9dfc9b815df58ca66645ffcc128acbc6
Enrique Ortiz <evalir@users.noreply.github.com>
2023-01-11 20:20:30 -0400

    chore: add compile_error on deprecated feature with-serde

--- crates/revm/src/lib.rs
+++ crates/revm/src/lib.rs
@@ -6,6 +6,9 @@ mod evm_impl;
 mod inspector;
 mod journaled_state;
 
+#[cfg(feature = "with-serde")]
+compile_error!("`with-serde` feature has been renamed to `serde`.");
+
 pub(crate) const USE_GAS: bool = !cfg!(feature = "no_gas_measuring");
 pub type DummyStateDB = InMemoryDB;
 
