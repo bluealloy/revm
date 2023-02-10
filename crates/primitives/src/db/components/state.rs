1c3e9e35c88d575924a3cfafcc6257d715118945
Wodann <Wodann@users.noreply.github.com>
2023-02-10 03:54:05 -0600

    improvement: use alloc & core for Arc impl (#367)

--- crates/primitives/src/db/components/state.rs
+++ crates/primitives/src/db/components/state.rs
@@ -2,7 +2,9 @@
 //! it is used inside [crate::db::DatabaseComponents`]
 
 use crate::{AccountInfo, Bytecode, B160, B256, U256};
+use alloc::sync::Arc;
 use auto_impl::auto_impl;
+use core::ops::Deref;
 
 #[auto_impl(& mut, Box)]
 pub trait State {
@@ -49,25 +51,21 @@ where
     }
 }
 
-#[cfg(feature = "std")]
-impl<T> State for std::sync::Arc<T>
+impl<T> State for Arc<T>
 where
     T: StateRef,
 {
     type Error = <T as StateRef>::Error;
 
     fn basic(&mut self, address: B160) -> Result<Option<AccountInfo>, Self::Error> {
-        use std::ops::Deref;
         self.deref().basic(address)
     }
 
     fn code_by_hash(&mut self, code_hash: B256) -> Result<Bytecode, Self::Error> {
-        use std::ops::Deref;
         self.deref().code_by_hash(code_hash)
     }
 
     fn storage(&mut self, address: B160, index: U256) -> Result<U256, Self::Error> {
-        use std::ops::Deref;
         self.deref().storage(address, index)
     }
 }
