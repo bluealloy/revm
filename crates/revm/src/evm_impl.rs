c2ee8ff5b4f0262565fa65a6a95c2a430116fa68
Dan Cline <6798349+Rjected@users.noreply.github.com>
2023-03-29 12:10:04 -0400

    add feature for ignoring base fee check (#436)

--- crates/revm/src/evm_impl.rs
+++ crates/revm/src/evm_impl.rs
@@ -60,10 +60,15 @@ impl<'a, GSPEC: Spec, DB: Database, const INSPECT: bool> Transact<DB::Error>
             }
             let basefee = self.data.env.block.basefee;
 
+            #[cfg(feature = "optional_no_base_fee")]
+            let disable_base_fee = self.env().cfg.disable_base_fee;
+            #[cfg(not(feature = "optional_no_base_fee"))]
+            let disable_base_fee = false;
+
             // check minimal cost against basefee
             // TODO maybe do this checks when creating evm. We already have all data there
             // or should be move effective_gas_price inside transact fn
-            if effective_gas_price < basefee {
+            if !disable_base_fee && effective_gas_price < basefee {
                 return Err(InvalidTransaction::GasPriceLessThanBasefee.into());
             }
             // check if priority fee is lower then max fee
