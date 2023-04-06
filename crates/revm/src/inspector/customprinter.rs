174b3ba52e72e1ca068c70e3a7c15728f4123d4e
omahs <73983677+omahs@users.noreply.github.com>
2023-04-06 12:11:38 +0200

    Fix: typos

--- crates/revm/src/inspector/customprinter.rs
+++ crates/revm/src/inspector/customprinter.rs
@@ -1,5 +1,5 @@
 //! Custom print inspector, it has step level information of execution.
-//! It is great tool if some debugging is needed.
+//! It is a great tool if some debugging is needed.
 //!
 use crate::interpreter::{opcode, CallInputs, CreateInputs, Gas, InstructionResult, Interpreter};
 use crate::primitives::{hex, Bytes, B160};
@@ -138,7 +138,7 @@ mod test {
     fn gas_calculation_underflow() {
         use crate::primitives::hex_literal;
         // https://github.com/bluealloy/revm/issues/277
-        // checks this usecase
+        // checks this use case
         let mut evm = crate::new();
         let mut database = crate::InMemoryDB::default();
         let code: crate::primitives::Bytes = hex_literal::hex!("5b597fb075978b6c412c64d169d56d839a8fe01b3f4607ed603b2c78917ce8be1430fe6101e8527ffe64706ecad72a2f5c97a95e006e279dc57081902029ce96af7edae5de116fec610208527f9fc1ef09d4dd80683858ae3ea18869fe789ddc365d8d9d800e26c9872bac5e5b6102285260276102485360d461024953601661024a53600e61024b53607d61024c53600961024d53600b61024e5360b761024f5360596102505360796102515360a061025253607261025353603a6102545360fb61025553601261025653602861025753600761025853606f61025953601761025a53606161025b53606061025c5360a661025d53602b61025e53608961025f53607a61026053606461026153608c6102625360806102635360d56102645360826102655360ae61026653607f6101e8610146610220677a814b184591c555735fdcca53617f4d2b9134b29090c87d01058e27e962047654f259595947443b1b816b65cdb6277f4b59c10a36f4e7b8658f5a5e6f5561").to_vec().into();
