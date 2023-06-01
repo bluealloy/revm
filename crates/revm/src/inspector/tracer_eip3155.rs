d5ddb72f0eb55a41f1c21576591ceea0a70ae717
perama-v <83961755+perama-v@users.noreply.github.com>
2023-06-01 15:49:04 +1000

    feat(revm): add post-tx state root to 3155 tracer

--- crates/revm/src/inspector/tracer_eip3155.rs
+++ crates/revm/src/inspector/tracer_eip3155.rs
@@ -2,12 +2,18 @@
 
 use crate::inspectors::GasInspector;
 use crate::interpreter::{CallInputs, CreateInputs, Gas, InstructionResult};
+use crate::primitives;
 use crate::primitives::{db::Database, hex, Bytes, B160};
 use crate::{evm_impl::EVMData, Inspector};
-use revm_interpreter::primitives::U256;
+use ethers_core::types::{H160, H256};
+use ethers_core::utils::rlp::{self, RlpStream};
+use plain_hasher::PlainHasher;
+use revm_interpreter::primitives::{Account, SpecId, B256, U256};
 use revm_interpreter::{opcode, Interpreter, Memory, Stack};
 use serde_json::json;
+use sha3::{Digest, Keccak256};
 use std::io::Write;
+use triehash::sec_trie_root;
 
 pub struct TracerEip3155 {
     output: Box<dyn Write>,
@@ -105,10 +111,19 @@ impl<DB: Database> Inspector<DB> for TracerEip3155 {
         self.gas_inspector
             .call_end(data, inputs, remaining_gas, ret, out.clone());
         // self.log_step(interp, data, is_static, eval);
+        let is_legacy = !SpecId::enabled(data.env.cfg.spec_id, primitives::SpecId::SPURIOUS_DRAGON);
+
         self.skip = true;
         if data.journaled_state.depth() == 0 {
+            let state_root = state_merkle_trie_root(
+                data.journaled_state
+                    .state
+                    .iter()
+                    .filter(|(_address, acc)| account_is_part_of_trie(acc, is_legacy))
+                    .map(|(k, v)| (*k, v.clone())),
+            );
             let log_line = json!({
-                //stateroot
+                "stateRoot": format!("0x{state_root:x}"),
                 "output": format!("{out:?}"),
                 "gasUser": format!("0x{:x}", self.gas_inspector.gas_remaining()),
                 //time
@@ -174,4 +189,63 @@ fn short_hex(b: U256) -> String {
     } else {
         format!("0x{s}")
     }
+}
+
+// (&B160, &revm_interpreter::revm_primitives::Account)
+pub fn state_merkle_trie_root(accounts: impl Iterator<Item = (B160, Account)>) -> B256 {
+    let vec = accounts
+        .map(|(address, info)| {
+            let acc_root = trie_account_rlp(&info);
+            (H160::from(address.0), acc_root)
+        })
+        .collect();
+
+    trie_root(vec)
+}
+
+/// Returns the RLP for this account.
+pub fn trie_account_rlp(acc: &Account) -> Bytes {
+    let mut stream = RlpStream::new_list(4);
+    stream.append(&acc.info.nonce);
+    stream.append(&acc.info.balance);
+    stream.append(&{
+        sec_trie_root::<KeccakHasher, _, _, _>(
+            acc.storage
+                .iter()
+                .filter(|(_k, &ref v)| v.original_value != U256::ZERO)
+                .map(|(&k, v)| (H256::from(k.to_be_bytes()), rlp::encode(&v.original_value))),
+        )
+    });
+    stream.append(&acc.info.code_hash.0.as_ref());
+    stream.out().freeze()
+}
+
+pub fn trie_root(acc_data: Vec<(H160, Bytes)>) -> B256 {
+    B256(sec_trie_root::<KeccakHasher, _, _, _>(acc_data.into_iter()).0)
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Eq)]
+pub struct KeccakHasher;
+
+impl hash_db::Hasher for KeccakHasher {
+    type Out = H256;
+    type StdHasher = PlainHasher;
+    const LENGTH: usize = 32;
+    fn hash(x: &[u8]) -> Self::Out {
+        let out = Keccak256::digest(x);
+        H256::from_slice(out.as_slice())
+    }
+}
+
+/// Determines if an account should be included as part of the trie root calculation.
+///
+/// - Pre-spurious-dragon: existing and empty status were two separate states.
+/// - Post-spurious-dragon: empty means non-existent.
+fn account_is_part_of_trie(acc: &Account, is_legacy: bool) -> bool {
+    // Pre-spurious-dragon: exists
+    (is_legacy && !acc.is_loaded_as_not_existing())
+    // Post-spurious-dragon: exists
+    || !is_legacy && (!(acc.info.is_empty())
+    // Pre- or post-spurious-dragon: does not exist.
+    || !acc.info.exists())
 }
