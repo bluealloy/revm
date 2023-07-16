a6b4b10238bb43865c4af8b9ec1e686aedd71165
Colin Roberts <colin@primitive.xyz>
2023-07-16 14:59:27 +0200

    tests :white_check_mark:

--- crates/precompile/src/point_evaluation.rs
+++ crates/precompile/src/point_evaluation.rs
@@ -59,3 +59,31 @@ pub fn kzg_to_versioned_hash(commitment: &[u8]) -> Vec<u8> {
 pub fn verify_kzg_proof(_commitment: &[u8], _z: &[u8], _y: &[u8], _proof: &[u8]) -> bool {
     todo!();
 }
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn kzg_to_versioned_hash() {
+        let commitment = [0x01, 0x02];
+        let hashed_commitment = super::kzg_to_versioned_hash(&commitment);
+        assert_eq!(
+            hashed_commitment,
+            [
+                1, 40, 113, 254, 226, 16, 251, 134, 25, 41, 30, 174, 161, 148, 88, 28, 189, 37, 49,
+                228, 178, 55, 89, 210, 37, 246, 128, 105, 35, 246, 50, 34
+            ]
+        );
+    }
+
+    #[test]
+    fn verify_kzg_proof() {
+        todo!();
+    }
+
+    #[test]
+    fn point_evaluation_run() {
+        // Test the assertion in the run too
+        todo!();
+    }
+}
