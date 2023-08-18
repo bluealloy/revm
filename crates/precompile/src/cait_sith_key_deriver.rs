6832031ed07c0d1a9a655ac52d8a3bfc4fb7c33c
Chris Cassano <1285652+glitch003@users.noreply.github.com>
2023-08-17 19:19:26 -0600

    added println

--- crates/precompile/src/cait_sith_key_deriver.rs
+++ crates/precompile/src/cait_sith_key_deriver.rs
@@ -1,5 +1,4 @@
 use super::calc_linear_cost_u32;
-use crate::primitives::U256;
 use crate::{Error, Precompile, PrecompileAddress, PrecompileResult, StandardPrecompileFn, Vec};
 use hd_keys_ecdsa::*;
 use k256::elliptic_curve::sec1::FromEncodedPoint;
@@ -29,6 +28,8 @@ fn derive_cait_sith_pubkey(input: &[u8], gas_limit: u64) -> PrecompileResult {
     // parse input into (bytes32 derivedKeyId, RootKey[] memory rootHDKeys)
 
     let derived_key_id = &input[0..32];
+    println!("derived_key_id: {:?}", derived_key_id);
+
     let root_hd_keys_data = &input[32..];
 
     let mut root_hd_keys = Vec::new();
@@ -41,14 +42,14 @@ fn derive_cait_sith_pubkey(input: &[u8], gas_limit: u64) -> PrecompileResult {
         let pubkey_len = pubkey_len as usize;
         i += 8;
         let pubkey = &root_hd_keys_data[i..i + pubkey_len];
-        i += 32;
-        let pubkey = &root_hd_keys_data[i..i + pubkey_len];
         i += pubkey_len;
         let key_type = &root_hd_keys_data[i..i + 32];
         i += 32;
-        let projective_point = bytes_to_projective_point(&pubkey);
+        let projective_point = bytes_to_projective_point(pubkey);
         root_hd_keys.push(projective_point);
     }
+
+    println!("root_hd_keys: {:?}", root_hd_keys);
 
     let deriver = HdKeyDeriver::<Secp256k1>::new(
         derived_key_id,
--- crates/precompile/src/lib.rs
+++ crates/precompile/src/lib.rs
@@ -1,4 +1,4 @@
-#![no_std]
+#![cfg_attr(not(feature = "std"), no_std)]
 
 #[macro_use]
 extern crate alloc;
