102615917997f36bf8ae10d93a4b3bfb081e8a4b
Dan Cline <6798349+Rjected@users.noreply.github.com>
2023-09-01 16:00:17 -0700

    chore: impl Eq, PartialEq for TransitionState (#677)
    * chore: impl Eq, PartialEq for TransitionState
    
    * re-add default impl with comment
    
    * remove conflicting Default impls

--- crates/revm/src/db/states/transition_state.rs
+++ crates/revm/src/db/states/transition_state.rs
@@ -1,7 +1,7 @@
 use super::TransitionAccount;
 use revm_interpreter::primitives::{hash_map::Entry, Address, HashMap};
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub struct TransitionState {
     /// Block state account with account state
     pub transitions: HashMap<Address, TransitionAccount>,
