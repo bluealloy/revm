3b28d2a599a0e93035f004a8b900926577c7862d
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2023-09-06 03:17:18 +0200

    chore: implement `Default` for other databases

--- crates/primitives/src/db.rs
+++ crates/primitives/src/db.rs
@@ -1,5 +1,3 @@
-pub mod components;
-
 use crate::AccountInfo;
 use crate::U256;
 use crate::{Account, Bytecode};
@@ -7,21 +5,26 @@ use crate::{B160, B256};
 use auto_impl::auto_impl;
 use hashbrown::HashMap as Map;
 
+pub mod components;
 pub use components::{
     BlockHash, BlockHashRef, DatabaseComponentError, DatabaseComponents, State, StateRef,
 };
 
+/// EVM database interface.
 #[auto_impl(&mut, Box)]
 pub trait Database {
     type Error;
+
     /// Get basic account information.
     fn basic(&mut self, address: B160) -> Result<Option<AccountInfo>, Self::Error>;
-    /// Get account code by its hash
+
+    /// Get account code by its hash.
     fn code_by_hash(&mut self, code_hash: B256) -> Result<Bytecode, Self::Error>;
+
     /// Get storage value of address at index.
     fn storage(&mut self, address: B160, index: U256) -> Result<U256, Self::Error>;
 
-    // History related
+    /// Get block hash by block number.
     fn block_hash(&mut self, number: U256) -> Result<B256, Self::Error>;
 }
 
@@ -36,39 +39,46 @@ pub trait DatabaseCommit {
     fn commit(&mut self, changes: Map<B160, Account>);
 }
 
+/// Same as [Database], but uses immutable references.
 #[auto_impl(&, Box, Arc)]
 pub trait DatabaseRef {
     type Error;
-    /// Whether account at address exists.
-    //fn exists(&self, address: B160) -> Option<AccountInfo>;
+
     /// Get basic account information.
     fn basic(&self, address: B160) -> Result<Option<AccountInfo>, Self::Error>;
-    /// Get account code by its hash
+
+    /// Get account code by its hash.
     fn code_by_hash(&self, code_hash: B256) -> Result<Bytecode, Self::Error>;
+
     /// Get storage value of address at index.
     fn storage(&self, address: B160, index: U256) -> Result<U256, Self::Error>;
 
-    // History related
+    /// Get block hash by block number.
     fn block_hash(&self, number: U256) -> Result<B256, Self::Error>;
 }
 
+/// Wraps a [`DatabaseRef`] to provide a [`Database`] implementation.
 pub struct WrapDatabaseRef<T: DatabaseRef>(pub T);
 
 impl<T: DatabaseRef> Database for WrapDatabaseRef<T> {
     type Error = T::Error;
 
+    #[inline]
     fn basic(&mut self, address: B160) -> Result<Option<AccountInfo>, Self::Error> {
         self.0.basic(address)
     }
 
+    #[inline]
     fn code_by_hash(&mut self, code_hash: B256) -> Result<Bytecode, Self::Error> {
         self.0.code_by_hash(code_hash)
     }
 
+    #[inline]
     fn storage(&mut self, address: B160, index: U256) -> Result<U256, Self::Error> {
         self.0.storage(address, index)
     }
 
+    #[inline]
     fn block_hash(&mut self, number: U256) -> Result<B256, Self::Error> {
         self.0.block_hash(number)
     }
--- crates/revm/src/db/emptydb.rs
+++ crates/revm/src/db/emptydb.rs
@@ -1,80 +1,88 @@
 use core::{convert::Infallible, marker::PhantomData};
 use revm_interpreter::primitives::{
     db::{Database, DatabaseRef},
-    keccak256, AccountInfo, Bytecode, B160, B256, U256,
+    AccountInfo, Bytecode, B160, B256, U256,
 };
 
+/// An empty database that always returns default values when queried.
 pub type EmptyDB = EmptyDBTyped<Infallible>;
 
-impl Default for EmptyDB {
+/// An empty database that always returns default values when queried.
+///
+/// This is generic over a type which is used as the database error type.
+#[derive(Debug, Clone)]
+pub struct EmptyDBTyped<E> {
+    _phantom: PhantomData<E>,
+}
+
+// Don't derive it because it doesn't need `E: Default`.
+impl<E> Default for EmptyDBTyped<E> {
+    #[inline(always)]
     fn default() -> Self {
-        Self {
-            keccak_block_hash: false,
-            _phantom: PhantomData,
-        }
+        Self::new()
     }
 }
 
-/// An empty database that always returns default values when queried.
-#[derive(Debug, Clone)]
-pub struct EmptyDBTyped<T> {
-    pub keccak_block_hash: bool,
-    pub _phantom: PhantomData<T>,
-}
-
-impl<T> EmptyDBTyped<T> {
+impl<E> EmptyDBTyped<E> {
+    #[inline(always)]
     pub fn new() -> Self {
         Self {
-            keccak_block_hash: false,
             _phantom: PhantomData,
         }
     }
 
+    #[doc(hidden)]
+    #[deprecated = "use `new` instead"]
+    #[inline(always)]
     pub fn new_keccak_block_hash() -> Self {
-        Self {
-            keccak_block_hash: true,
-            _phantom: PhantomData,
-        }
+        Self::new()
     }
 }
 
-impl<T> Database for EmptyDBTyped<T> {
-    type Error = T;
+impl<E> Database for EmptyDBTyped<E> {
+    type Error = E;
 
+    #[inline(always)]
     fn basic(&mut self, address: B160) -> Result<Option<AccountInfo>, Self::Error> {
         <Self as DatabaseRef>::basic(self, address)
     }
 
+    #[inline(always)]
     fn code_by_hash(&mut self, code_hash: B256) -> Result<Bytecode, Self::Error> {
         <Self as DatabaseRef>::code_by_hash(self, code_hash)
     }
 
+    #[inline(always)]
     fn storage(&mut self, address: B160, index: U256) -> Result<U256, Self::Error> {
         <Self as DatabaseRef>::storage(self, address, index)
     }
 
+    #[inline(always)]
     fn block_hash(&mut self, number: U256) -> Result<B256, Self::Error> {
         <Self as DatabaseRef>::block_hash(self, number)
     }
 }
 
-impl<T> DatabaseRef for EmptyDBTyped<T> {
-    type Error = T;
-    /// Get basic account information.
+impl<E> DatabaseRef for EmptyDBTyped<E> {
+    type Error = E;
+
+    #[inline(always)]
     fn basic(&self, _address: B160) -> Result<Option<AccountInfo>, Self::Error> {
         Ok(None)
     }
-    /// Get account code by its hash
+
+    #[inline(always)]
     fn code_by_hash(&self, _code_hash: B256) -> Result<Bytecode, Self::Error> {
         Ok(Bytecode::new())
     }
-    /// Get storage value of address at index.
+
+    #[inline(always)]
     fn storage(&self, _address: B160, _index: U256) -> Result<U256, Self::Error> {
         Ok(U256::default())
     }
 
-    // History related
+    #[inline(always)]
     fn block_hash(&self, number: U256) -> Result<B256, Self::Error> {
-        Ok(keccak256(&number.to_be_bytes::<{ U256::BYTES }>()))
+        Ok(number.to_be_bytes().into())
     }
 }
