9343bf70b6518a4903ebfc1ed762aa1bf7fc192d
Lorenzo Feroleto <60553286+lorenzofero@users.noreply.github.com>
2023-09-07 15:33:03 +0200

    chore(cfg): convert chain_id from u256 to u64 (#693)

--- crates/interpreter/src/instructions/host_env.rs
+++ crates/interpreter/src/instructions/host_env.rs
@@ -1,3 +1,5 @@
+use revm_primitives::U256;
+
 use crate::{
     gas, interpreter::Interpreter, primitives::Spec, primitives::SpecId::*, Host, InstructionResult,
 };
@@ -6,7 +8,7 @@ pub fn chainid<SPEC: Spec>(interpreter: &mut Interpreter, host: &mut dyn Host) {
     // EIP-1344: ChainID opcode
     check!(interpreter, SPEC::enabled(ISTANBUL));
     gas!(interpreter, gas::BASE);
-    push!(interpreter, host.env().cfg.chain_id);
+    push!(interpreter, U256::from(host.env().cfg.chain_id));
 }
 
 pub fn coinbase(interpreter: &mut Interpreter, host: &mut dyn Host) {
