rising9719/revm-v-rust

fix(state): retain destroyed account status on bundle extend (#667)
* fix(state): retain destroyed account status on bundle extend

* Move extend functionality to account status

crates/revm/src/db/states/bundle_state.rs
@@ -365,8 +365,8 @@ impl BundleState {
                                 .present_value = storage_slot.present_value;
                         }
                     }
-                    this.status = other_account.status;
                     this.info = other_account.info;
+                    this.status.transition(other_account.status);
                 }
                 hash_map::Entry::Vacant(entry) => {
                     // just insert if empty
@@ -481,12 +481,12 @@ mod tests {
         );
     }
 
-    fn account1() -> B160 {
-        [0x60; 20].into()
+    const fn account1() -> B160 {
+        B160([0x60; 20])
     }
 
-    fn account2() -> B160 {
-        [0x61; 20].into()
+    const fn account2() -> B160 {
+        B160([0x61; 20])
     }
 
     fn slot() -> U256 {
@@ -584,5 +584,54 @@ mod tests {
         // reverted by bigger number gives us empty bundle
         reverted.revert(10);
         assert_eq!(reverted, BundleState::default());
+    }
+
+    #[test]
+    fn extend_on_destoyed_values() {
+        let base_bundle1 = test_bundle1();
+        let base_bundle2 = test_bundle2();
+
+        // test1
+        // bundle1 has Destroyed
+        // bundle2 has Changed
+        // end should be DestroyedChanged.
+        let mut b1 = base_bundle1.clone();
+        let mut b2 = base_bundle2.clone();
+        b1.state.get_mut(&account1()).unwrap().status = AccountStatus::Destroyed;
+        b2.state.get_mut(&account1()).unwrap().status = AccountStatus::Changed;
+        b1.extend(b2);
+        assert_eq!(
+            b1.state.get_mut(&account1()).unwrap().status,
+            AccountStatus::DestroyedChanged
+        );
+
+        // test2
+        // bundle1 has Changed
+        // bundle2 has Destroyed
+        // end should be Destroyed
+        let mut b1 = base_bundle1.clone();
+        let mut b2 = base_bundle2.clone();
+        b1.state.get_mut(&account1()).unwrap().status = AccountStatus::Changed;
+        b2.state.get_mut(&account1()).unwrap().status = AccountStatus::Destroyed;
+        b1.extend(b2);
+        assert_eq!(
+            b1.state.get_mut(&account1()).unwrap().status,
+            AccountStatus::Destroyed
+        );
+
+        // test3
+        // bundle1 has InMemoryChange
+        // bundle2 has Change
+        // end should be InMemoryChange.
+
+        let mut b1 = base_bundle1.clone();
+        let mut b2 = base_bundle2.clone();
+        b1.state.get_mut(&account1()).unwrap().status = AccountStatus::InMemoryChange;
+        b2.state.get_mut(&account1()).unwrap().status = AccountStatus::Changed;
+        b1.extend(b2);
+        assert_eq!(
+            b1.state.get_mut(&account1()).unwrap().status,
+            AccountStatus::InMemoryChange
+        );
     }
 }

bluealloy/revm

crates/revm/src/db/states/bundle_state.rs
@@ -365,8 +365,8 @@ impl BundleState {
                                 .present_value = storage_slot.present_value;
                         }
                     }
-                    this.status = other_account.status;
                     this.info = other_account.info;
+                    this.status.transition(other_account.status);
                 }
                 hash_map::Entry::Vacant(entry) => {
                     // just insert if empty
@@ -448,7 +448,7 @@ impl BundleState {
 mod tests {
     use super::*;
     use crate::{db::StorageWithOriginalValues, TransitionAccount};
-    use revm_interpreter::primitives::KECCAK_EMPTY;
+    use revm_interpreter::primitives::{KECCAK_EMPTY, FixedBytes};
 
     #[test]
     fn transition_states() {
@@ -481,12 +481,12 @@ mod tests {
         );
     }
 
-    fn account1() -> Address {
-        [0x60; 20].into()
+    const fn account1() -> Address {
+        Address(FixedBytes([0x60; 20]))
     }
 
-    fn account2() -> Address {
-        [0x61; 20].into()
+    const fn account2() -> Address {
+        Address(FixedBytes([0x60; 20]))
     }
 
     fn slot() -> U256 {
@@ -584,5 +584,54 @@ mod tests {
         // reverted by bigger number gives us empty bundle
         reverted.revert(10);
         assert_eq!(reverted, BundleState::default());
+    }
+
+    #[test]
+    fn extend_on_destoyed_values() {
+        let base_bundle1 = test_bundle1();
+        let base_bundle2 = test_bundle2();
+
+        // test1
+        // bundle1 has Destroyed
+        // bundle2 has Changed
+        // end should be DestroyedChanged.
+        let mut b1 = base_bundle1.clone();
+        let mut b2 = base_bundle2.clone();
+        b1.state.get_mut(&account1()).unwrap().status = AccountStatus::Destroyed;
+        b2.state.get_mut(&account1()).unwrap().status = AccountStatus::Changed;
+        b1.extend(b2);
+        assert_eq!(
+            b1.state.get_mut(&account1()).unwrap().status,
+            AccountStatus::DestroyedChanged
+        );
+
+        // test2
+        // bundle1 has Changed
+        // bundle2 has Destroyed
+        // end should be Destroyed
+        let mut b1 = base_bundle1.clone();
+        let mut b2 = base_bundle2.clone();
+        b1.state.get_mut(&account1()).unwrap().status = AccountStatus::Changed;
+        b2.state.get_mut(&account1()).unwrap().status = AccountStatus::Destroyed;
+        b1.extend(b2);
+        assert_eq!(
+            b1.state.get_mut(&account1()).unwrap().status,
+            AccountStatus::Destroyed
+        );
+
+        // test3
+        // bundle1 has InMemoryChange
+        // bundle2 has Change
+        // end should be InMemoryChange.
+
+        let mut b1 = base_bundle1.clone();
+        let mut b2 = base_bundle2.clone();
+        b1.state.get_mut(&account1()).unwrap().status = AccountStatus::InMemoryChange;
+        b2.state.get_mut(&account1()).unwrap().status = AccountStatus::Changed;
+        b1.extend(b2);
+        assert_eq!(
+            b1.state.get_mut(&account1()).unwrap().status,
+            AccountStatus::InMemoryChange
+        );
     }
 }