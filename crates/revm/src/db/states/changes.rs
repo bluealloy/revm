rising9719/revm-v-rust

feat(state): take N reverts from BundleState, struct refactor (#681)
* feat(state): take N reverts from BundleState

* new line

* nits, export and PartialEq,Eq

* ftm

crates/revm/src/db/states/changes.rs
@@ -10,26 +10,49 @@ pub struct StateChangeset {
     pub accounts: Vec<(B160, Option<AccountInfo>)>,
     /// Vector of storage presorted by address
     /// First bool is indicator if storage needs to be dropped.
-    pub storage: StorageChangeset,
+    pub storage: Vec<PlainStorageChangeset>,
     /// Vector of contracts presorted by bytecode hash
     pub contracts: Vec<(B256, Bytecode)>,
 }
 
-/// Storage changeset
-pub type StorageChangeset = Vec<(B160, (bool, Vec<(U256, U256)>))>;
+/// Plain storage changeset. Used to apply storage changes of plain state to
+/// the database.
+#[derive(Clone, Debug, PartialEq, Eq, Default)]
+pub struct PlainStorageChangeset {
+    /// Address of account
+    pub address: B160,
+    /// Storage key value pairs.
+    pub storage: Vec<(U256, U256)>,
+}
 
+/// Plain Storage Revert. Containing old values of changed storage.
+#[derive(Clone, Debug, PartialEq, Eq, Default)]
+pub struct PlainStorageRevert {
+    /// Address of account
+    pub address: B160,
+    /// Is storage wiped in this revert. Wiped flag is set on
+    /// first known selfdestruct and would require clearing the
+    /// state of this storage from database (And moving it to revert).
+    pub wiped: bool,
+    /// Contains the storage key and old values of that storage.
+    /// Assume they are sorted by the key.
+    pub storage_revert: Vec<(U256, RevertToSlot)>,
+}
+
+/// Plain state reverts are used to easily store reverts into database.
+///
+/// Note that accounts are assumed sorted by address.
 #[derive(Clone, Debug, Default)]
-pub struct StateReverts {
+pub struct PlainStateReverts {
     /// Vector of account presorted by address, with removed contracts bytecode
     ///
     /// Note: AccountInfo None means that account needs to be removed.
     pub accounts: Vec<Vec<(B160, Option<AccountInfo>)>>,
     /// Vector of storage presorted by address
-    /// U256::ZERO means that storage needs to be removed.
-    pub storage: StorageRevert,
+    pub storage: Vec<Vec<PlainStorageRevert>>,
 }
 
-impl StateReverts {
+impl PlainStateReverts {
     /// Constructs new [StateReverts] with pre-allocated capacity.
     pub fn with_capacity(capacity: usize) -> Self {
         Self {

bluealloy/revm

crates/revm/src/db/states/changes.rs
@@ -10,26 +10,49 @@ pub struct StateChangeset {
     pub accounts: Vec<(Address, Option<AccountInfo>)>,
     /// Vector of storage presorted by address
     /// First bool is indicator if storage needs to be dropped.
-    pub storage: StorageChangeset,
+    pub storage: Vec<PlainStorageChangeset>,
     /// Vector of contracts presorted by bytecode hash
     pub contracts: Vec<(B256, Bytecode)>,
 }
 
-/// Storage changeset
-pub type StorageChangeset = Vec<(Address, (bool, Vec<(U256, U256)>))>;
+/// Plain storage changeset. Used to apply storage changes of plain state to
+/// the database.
+#[derive(Clone, Debug, PartialEq, Eq, Default)]
+pub struct PlainStorageChangeset {
+    /// Address of account
+    pub address: Address,
+    /// Storage key value pairs.
+    pub storage: Vec<(U256, U256)>,
+}
 
+/// Plain Storage Revert. Containing old values of changed storage.
+#[derive(Clone, Debug, PartialEq, Eq, Default)]
+pub struct PlainStorageRevert {
+    /// Address of account
+    pub address: Address,
+    /// Is storage wiped in this revert. Wiped flag is set on
+    /// first known selfdestruct and would require clearing the
+    /// state of this storage from database (And moving it to revert).
+    pub wiped: bool,
+    /// Contains the storage key and old values of that storage.
+    /// Assume they are sorted by the key.
+    pub storage_revert: Vec<(U256, RevertToSlot)>,
+}
+
+/// Plain state reverts are used to easily store reverts into database.
+///
+/// Note that accounts are assumed sorted by address.
 #[derive(Clone, Debug, Default)]
-pub struct StateReverts {
+pub struct PlainStateReverts {
     /// Vector of account presorted by address, with removed contracts bytecode
     ///
     /// Note: AccountInfo None means that account needs to be removed.
     pub accounts: Vec<Vec<(Address, Option<AccountInfo>)>>,
     /// Vector of storage presorted by address
-    /// U256::ZERO means that storage needs to be removed.
-    pub storage: StorageRevert,
+    pub storage: Vec<Vec<PlainStorageRevert>>,
 }
 
-impl StateReverts {
+impl PlainStateReverts {
     /// Constructs new [StateReverts] with pre-allocated capacity.
     pub fn with_capacity(capacity: usize) -> Self {
         Self {