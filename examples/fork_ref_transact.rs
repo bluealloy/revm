rising9719/revm-v-rust

fix example compile error (#695)

examples/fork_ref_transact.rs
@@ -80,7 +80,7 @@ async fn main() -> Result<()> {
     // calldata formed via abigen
     evm.env.tx.data = Bytes::from(hex::decode(hex::encode(&encoded))?);
     // transaction value in wei
-    evm.env.tx.value = rU256::try_from(0)?;
+    evm.env.tx.value = rU256::from(0);
 
     // execute transaction without writing to the DB
     let ref_tx = evm.transact_ref().unwrap();

bluealloy/revm

examples/fork_ref_transact.rs
@@ -78,7 +78,7 @@ async fn main() -> anyhow::Result<()> {
     // calldata formed via abigen
     evm.env.tx.data = encoded.0.into();
     // transaction value in wei
-    evm.env.tx.value = U256::ZERO;
+    evm.env.tx.value = U256::from(0);
 
     // execute transaction without writing to the DB
     let ref_tx = evm.transact_ref().unwrap();