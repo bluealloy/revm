09f5e155841c31262bdf910e2dac7614dcc54cfe
Enrique Ortiz <hi@enriqueortiz.dev>
2023-09-25 17:07:18 -0400

    feat: make c-kzg not default on primitives

--- crates/primitives/src/kzg.rs
+++ crates/primitives/src/kzg.rs
@@ -2,8 +2,11 @@ mod env_settings;
 #[rustfmt::skip]
 mod generated;
 
+#[cfg(feature = "c_kzg")]
 pub use c_kzg::KzgSettings;
+#[cfg(feature = "c_kzg")]
 pub use env_settings::EnvKzgSettings;
+#[cfg(feature = "c_kzg")]
 pub use generated::{
     BYTES_PER_G1_POINT, BYTES_PER_G2_POINT, G1_POINTS, G2_POINTS, NUM_G1_POINTS, NUM_G2_POINTS,
 };
