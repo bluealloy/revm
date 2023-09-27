1baa7eb4dcbb59dba54b8207e25c183189549086
rakita <rakita@users.noreply.github.com>
2023-09-27 16:15:34 +0200

    Update crates/primitives/src/state.rs

--- crates/primitives/src/state.rs
+++ crates/primitives/src/state.rs
@@ -173,7 +173,7 @@ pub struct AccountInfo {
     /// code hash,
     pub code_hash: B256,
     /// code: if None, `code_by_hash` will be used to fetch it if code needs to be loaded from
-    /// inside of REVM.
+    /// inside of `revm`.
     pub code: Option<Bytecode>,
 }
 
