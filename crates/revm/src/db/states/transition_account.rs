3bebae8c93edc3d789e13c7383725358e49d5815
Roman Krasiuk <rokrassyuk@gmail.com>
2023-10-27 06:23:29 -0700

    feat: transition account balance delta (#843)

--- crates/revm/src/db/states/transition_account.rs
+++ crates/revm/src/db/states/transition_account.rs
@@ -1,6 +1,6 @@
 use super::{AccountRevert, BundleAccount, StorageWithOriginalValues};
 use crate::db::AccountStatus;
-use revm_interpreter::primitives::{hash_map, AccountInfo, Bytecode, B256};
+use revm_interpreter::primitives::{hash_map, AccountInfo, Bytecode, B256, I256, U256};
 
 /// Account Created when EVM state is merged to cache state.
 /// And it is sent to Block state.
@@ -51,6 +51,35 @@ impl TransitionAccount {
                 .and_then(|info| info.code.as_ref().map(|c| (info.code_hash, c)));
         }
         None
+    }
+
+    /// Return the balance of account before transition.
+    pub fn previous_balance(&self) -> U256 {
+        self.previous_info
+            .as_ref()
+            .map(|info| info.balance)
+            .unwrap_or_default()
+    }
+
+    /// Return the balance of account after transition.
+    pub fn current_balance(&self) -> U256 {
+        self.info
+            .as_ref()
+            .map(|info| info.balance)
+            .unwrap_or_default()
+    }
+
+    /// Calculate the change in account's balance for this transition.
+    /// Returns `None` if delta does not fit in [I256].
+    pub fn balance_delta(&self) -> Option<I256> {
+        let previous_balance = self.previous_balance();
+        let current_balance = self.current_balance();
+        let delta = I256::try_from(previous_balance.abs_diff(current_balance)).ok()?;
+        if current_balance >= previous_balance {
+            Some(delta)
+        } else {
+            delta.checked_neg()
+        }
     }
 
     /// Update new values of transition. Don't override old values.
