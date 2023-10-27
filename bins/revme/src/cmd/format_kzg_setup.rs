37ee69354e5238d0f8a25817dbcd51ee509853f0
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2023-10-27 15:41:58 +0200

    perf: decode KZG points directly into the buffers (#840)

--- bins/revme/src/cmd/format_kzg_setup.rs
+++ bins/revme/src/cmd/format_kzg_setup.rs
@@ -1,4 +1,4 @@
-pub use revm::primitives::kzg::{format_kzg_settings, G1Points, G2Points, KzgErrors};
+pub use revm::primitives::kzg::{parse_kzg_trusted_setup, G1Points, G2Points, KzgErrors};
 use std::path::PathBuf;
 use std::{env, fs};
 use structopt::StructOpt;
@@ -31,7 +31,7 @@ impl Cmd {
             fs::read_to_string(&self.path).map_err(|_| KzgErrors::NotValidFile)?;
 
         // format points
-        let (g1, g2) = format_kzg_settings(&kzg_trusted_settings)?;
+        let (g1, g2) = parse_kzg_trusted_setup(&kzg_trusted_settings)?;
 
         let g1_path = self
             .g1
@@ -44,44 +44,17 @@ impl Cmd {
             .unwrap_or_else(|| out_dir.join("g2_points.bin"));
 
         // output points
-        fs::write(&g1_path, into_flattened(g1.to_vec())).map_err(|_| KzgErrors::IOError)?;
-        fs::write(&g2_path, into_flattened(g2.to_vec())).map_err(|_| KzgErrors::IOError)?;
+        fs::write(&g1_path, flatten(&g1.0)).map_err(|_| KzgErrors::IOError)?;
+        fs::write(&g2_path, flatten(&g2.0)).map_err(|_| KzgErrors::IOError)?;
         println!("Finished formatting kzg trusted setup into binary representation.");
-        println!("G1 point path: {:?}", g1_path);
-        println!("G2 point path: {:?}", g2_path);
+        println!("G1 points path: {:?}", g1_path);
+        println!("G2 points path: {:?}", g2_path);
         Ok(())
     }
 }
 
-/// [`Vec::into_flattened`].
-fn into_flattened<T, const N: usize>(vec: Vec<[T; N]>) -> Vec<T> {
-    let (ptr, len, cap) = into_raw_parts(vec);
-    let (new_len, new_cap) = if core::mem::size_of::<T>() == 0 {
-        (len.checked_mul(N).expect("vec len overflow"), usize::MAX)
-    } else {
-        // SAFETY:
-        // - `cap * N` cannot overflow because the allocation is already in
-        // the address space.
-        // - Each `[T; N]` has `N` valid elements, so there are `len * N`
-        // valid elements in the allocation.
-        unsafe {
-            (
-                len.checked_mul(N).unwrap_unchecked(),
-                cap.checked_mul(N).unwrap_unchecked(),
-            )
-        }
-    };
-    // SAFETY:
-    // - `ptr` was allocated by `self`
-    // - `ptr` is well-aligned because `[T; N]` has the same alignment as `T`.
-    // - `new_cap` refers to the same sized allocation as `cap` because
-    // `new_cap * size_of::<T>()` == `cap * size_of::<[T; N]>()`
-    // - `len` <= `cap`, so `len * N` <= `cap * N`.
-    unsafe { Vec::from_raw_parts(ptr.cast(), new_len, new_cap) }
-}
-
-/// [`Vec::into_raw_parts`]
-fn into_raw_parts<T>(vec: Vec<T>) -> (*mut T, usize, usize) {
-    let mut me = core::mem::ManuallyDrop::new(vec);
-    (me.as_mut_ptr(), me.len(), me.capacity())
+fn flatten<const N: usize, const M: usize>(x: &[[u8; N]; M]) -> &[u8] {
+    // SAFETY: `x` is a valid `[[u8; N]; M]` and `N * M` is the length of the
+    // returned slice.
+    unsafe { core::slice::from_raw_parts(x.as_ptr().cast(), N * M) }
 }
