361fbd013f55c3b6cdf9049a352d4b8ce8093417
refcell <abigger87@gmail.com>
2023-12-02 15:19:06 -0800

    fix(crates): move inline lint reporting levels to the package manifest [lints] table

--- crates/interpreter/src/lib.rs
+++ crates/interpreter/src/lib.rs
@@ -1,8 +1,6 @@
 //! # revm-interpreter
 //!
 //! REVM Interpreter.
-#![warn(unreachable_pub, unused_crate_dependencies)]
-#![deny(unused_must_use, rust_2018_idioms)]
 #![cfg_attr(not(feature = "std"), no_std)]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
 
--- crates/precompile/src/lib.rs
+++ crates/precompile/src/lib.rs
@@ -1,8 +1,6 @@
 //! # revm-precompile
 //!
 //! Implementations of EVM precompiled contracts.
-#![warn(unused_crate_dependencies)]
-#![deny(unused_must_use, rust_2018_idioms)]
 #![cfg_attr(not(feature = "std"), no_std)]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
 
