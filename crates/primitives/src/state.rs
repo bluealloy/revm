42caf5537f155bddb18dc4b4abfccc478519881c
Iaroslav Mazur <iaroslav.mazur@proton.me>
2023-12-25 11:20:47 +0100

    perf(primitives): optimize AccountInfo.is_empty() (#922)
    * perf(primitives): optimize AccountInfo.is_empty()
    
    This is a small optimization for AccountInfo.is_empty() with
    regards to the short-circuiting of bool operators in Rust.
    
    * fix(primitives): incorrect use of the cfg attribute

--- crates/primitives/src/state.rs
+++ crates/primitives/src/state.rs
@@ -239,7 +239,7 @@ impl AccountInfo {
     /// - nonce is zero
     pub fn is_empty(&self) -> bool {
         let code_empty = self.is_empty_code_hash() || self.code_hash == B256::ZERO;
-        self.balance == U256::ZERO && self.nonce == 0 && code_empty
+        code_empty && self.balance == U256::ZERO && self.nonce == 0
     }
 
     /// Returns `true` if the account is not empty.
