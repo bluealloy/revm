a2dd2c5e328ec7dc1f1722869e81a364f43c8551
rakita <rakita@users.noreply.github.com>
2024-02-08 10:45:20 +0100

    tests(revme): statetests new format and return error (#1066)
    * tests(revme): statetests new format and return error
    
    * Include README

--- bins/revme/src/main.rs
+++ bins/revme/src/main.rs
@@ -1,9 +1,7 @@
-use revme::cmd::MainCmd;
+use revme::cmd::{Error, MainCmd};
 use structopt::StructOpt;
 
-pub fn main() {
+pub fn main() -> Result<(), Error> {
     let cmd = MainCmd::from_args();
-    if let Err(e) = cmd.run() {
-        println!("{}", e)
-    }
+    cmd.run()
 }
