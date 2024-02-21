1618d1be2b16a7310638e4b99a42e55c580114d2
rakita <dragan0rakita@gmail.com>
2024-02-21 02:51:00 +0100

    rm nightly items

--- crates/precompile/src/lib.rs
+++ crates/precompile/src/lib.rs
@@ -5,7 +5,6 @@
 #![warn(unused_crate_dependencies)]
 #![deny(unused_must_use, rust_2018_idioms)]
 #![cfg_attr(not(feature = "std"), no_std)]
-#![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
 
 #[macro_use]
 #[cfg(not(feature = "std"))]
--- crates/primitives/src/lib.rs
+++ crates/primitives/src/lib.rs
@@ -5,7 +5,6 @@
 #![warn(unreachable_pub, unused_crate_dependencies)]
 #![deny(unused_must_use, rust_2018_idioms)]
 #![cfg_attr(not(feature = "std"), no_std)]
-#![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
 
 #[cfg(not(feature = "std"))]
 extern crate alloc as std;
