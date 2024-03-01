a89daefa6c4cc237f8415cf9b8feb3e3cd2f675c
Charles Cooper <cooper.charles.m@gmail.com>
2024-02-29 20:08:29 -0800

    feat: add evm script (#1039)
    * feat: add evm script
    
    add a script which can run arbitrary binaries
    
    * fix lint
    
    * move evm runner script to `revme` subcommand
    
    * cargo fmt
    
    * cli byecode,input,path, state options added, output result

--- bins/revme/src/cmd.rs
+++ bins/revme/src/cmd.rs
@@ -1,3 +1,4 @@
+pub mod evmrunner;
 pub mod format_kzg_setup;
 pub mod statetest;
 
@@ -13,6 +14,10 @@ pub enum MainCmd {
         about = "Format kzg settings from a trusted setup file (.txt) into binary format (.bin)"
     )]
     FormatKzgSetup(format_kzg_setup::Cmd),
+    #[structopt(
+        about = "Evm runner command allows running arbitrary evm bytecode.\nBytecode can be provided from cli or from file with --path option."
+    )]
+    Evm(evmrunner::Cmd),
 }
 
 #[derive(Debug, thiserror::Error)]
@@ -21,6 +26,8 @@ pub enum Error {
     Statetest(#[from] statetest::Error),
     #[error(transparent)]
     KzgErrors(#[from] format_kzg_setup::KzgErrors),
+    #[error(transparent)]
+    EvmRunnerErrors(#[from] evmrunner::Errors),
 }
 
 impl MainCmd {
@@ -28,6 +35,7 @@ impl MainCmd {
         match self {
             Self::Statetest(cmd) => cmd.run().map_err(Into::into),
             Self::FormatKzgSetup(cmd) => cmd.run().map_err(Into::into),
+            Self::Evm(cmd) => cmd.run().map_err(Into::into),
         }
     }
 }
