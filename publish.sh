abc00d79772f4b5eb7787fd700ddcc2e60afd0f3
rakita <dragan0rakita@gmail.com>
2024-04-02 22:00:18 +0200

    chore: fix publish revm-precompile

--- publish.sh
+++ publish.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# stop on error
+set -e
+
 cargo publish --package revm-primitives
-cargo publish --package revm-precompiles
+cargo publish --package revm-precompile
 cargo publish --package revm-interpreter
 cargo publish --package revm
 cargo publish --package revme
