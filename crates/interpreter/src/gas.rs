e9b9b96ba7be52b053af7d4f6e1b6aa2cd86790a
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2024-04-03 10:10:29 +0200

    chore: use `is_london: bool` instead of `SpecId` in `Gas::set_final_refund`

--- crates/interpreter/src/gas.rs
+++ crates/interpreter/src/gas.rs
@@ -5,7 +5,6 @@ mod constants;
 
 pub use calc::*;
 pub use constants::*;
-use revm_primitives::SpecId;
 
 /// Represents the state of gas during execution.
 #[derive(Clone, Copy, Debug, Default, PartialEq, Eq, Hash)]
@@ -94,12 +93,8 @@ impl Gas {
     ///
     /// Related to EIP-3529: Reduction in refunds
     #[inline]
-    pub fn set_final_refund(&mut self, spec: SpecId) {
-        let max_refund_quotient = if spec.is_enabled_in(SpecId::LONDON) {
-            5
-        } else {
-            2
-        };
+    pub fn set_final_refund(&mut self, is_london: bool) {
+        let max_refund_quotient = if is_london { 5 } else { 2 };
         self.refunded = (self.refunded() as u64).min(self.spent() / max_refund_quotient) as i64;
     }
 
