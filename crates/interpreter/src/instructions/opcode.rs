6aba056348d2a8f9dfb452c276ca5e400d56ad57
h3lio5 <akashsm@ce.iitr.ac.in>
2024-04-07 14:56:29 +0530

    feat: add modifies_memory macro

--- crates/interpreter/src/instructions/opcode.rs
+++ crates/interpreter/src/instructions/opcode.rs
@@ -498,6 +498,28 @@ impl OpCode {
     }
 }
 
+/// Returns true if the opcode modifies memory.
+/// <https://bluealloy.github.io/revm/crates/interpreter/memory.html#opcodes>
+/// <https://github.com/crytic/evm-opcodes>
+#[inline]
+pub const fn modifies_memory(opcode: OpCode) -> bool {
+    matches!(
+        opcode,
+        OpCode::EXTCODECOPY
+            | OpCode::MLOAD
+            | OpCode::MSTORE
+            | OpCode::MSTORE8
+            | OpCode::MCOPY
+            | OpCode::CODECOPY
+            | OpCode::CALLDATACOPY
+            | OpCode::RETURNDATACOPY
+            | OpCode::CALL
+            | OpCode::CALLCODE
+            | OpCode::DELEGATECALL
+            | OpCode::STATICCALL
+    )
+}
+
 #[derive(Clone, Copy, Debug, Default, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct OpInfo {
     /// Data contains few information packed inside u32:
