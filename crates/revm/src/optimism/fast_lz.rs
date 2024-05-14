18a203f2940eeddf9a82c3fbc57ca8022e77519f
Brian Bland <brian.t.bland@gmail.com>
2024-05-14 16:06:32 -0700

    Adds a second, more faithful adaptation of solady fastlz for fuzzing

--- crates/revm/src/optimism/fast_lz.rs
+++ crates/revm/src/optimism/fast_lz.rs
@@ -107,8 +107,115 @@ fn u24(input: &[u8], idx: u32) -> u32 {
         + (u32::from(input[(idx + 2) as usize]) << 16)
 }
 
+/// This is a more faithful adaptation of the solady implementation of FastLZ, returning only the compressed size.
+/// Unfortunately this is even harder to read due to the use of nearly-minified javascript.
+// https://github.com/Vectorized/solady/blob/5315d937d79b335c668896d7533ac603adac5315/js/solady.js
+fn solady_flz_compress(ib: &[u8]) -> u32 {
+    let b: u32 = (ib.len() - 4) as u32;
+    let mut ht = [0; 8192];
+    let mut ob = vec![];
+    let mut a = 0;
+    let mut i = 2;
+    let mut r;
+    let mut s;
+    let mut d;
+    let m = 0xffffff;
+
+    let read_u32 = |i: u32| -> u32 {
+        u32::from(ib[i as usize])
+            | (u32::from(ib[(i + 1) as usize]) << 8)
+            | (u32::from(ib[(i + 2) as usize]) << 16)
+            | (u32::from(ib[(i + 3) as usize]) << 24)
+    };
+
+    fn hash(x: u32) -> u32 {
+        (((2654435769 * x as u64) >> 19) & 8191) as u32
+    };
+
+    fn literals(mut r: u32, mut s: u32, ib: &[u8], ob: &mut Vec<u8>) {
+        while r >= 32 {
+            ob.push(31);
+            let mut j = 32;
+            while j > 0 {
+                ob.push(ib[s as usize]);
+                s += 1;
+                j -= 1;
+                r -= 1;
+            }
+        }
+        if r > 0 {
+            ob.push((r - 1) as u8);
+            while r > 0 {
+                ob.push(ib[s as usize]);
+                s += 1;
+                r -= 1;
+            }
+        };
+    };
+
+    while i < b - 9 {
+        loop {
+            s = read_u32(i) & m;
+            let h = hash(s);
+            r = ht[h as usize];
+            ht[h as usize] = i;
+            d = i - r;
+            let c = if d < 8192 { read_u32(r) & m } else { m + 1 };
+            if i >= b - 9 {
+                break;
+            };
+            i += 1;
+            if s == c {
+                break;
+            };
+        }
+        if i >= b - 9 {
+            break;
+        };
+        i -= 1;
+        if i > a {
+            literals(i - a, a, ib, &mut ob);
+        };
+        let mut l = 0;
+        let p = r + 3;
+        let q = i + 3;
+        let mut e = b - q;
+        while l < e {
+            if ib[(p + l) as usize] != ib[(q + l) as usize] {
+                e = 0;
+            }
+            l += 1;
+        }
+        i += l;
+        s = read_u32(i);
+        d -= 1;
+        while l > 262 {
+            ob.push(224 + (d >> 8) as u8);
+            ob.push(253);
+            ob.push(d as u8);
+            l -= 262;
+        }
+        if l < 7 {
+            ob.push(((l << 5) + (d >> 8)) as u8);
+            ob.push(d as u8);
+        } else {
+            ob.push(224 + (d >> 8) as u8);
+            ob.push((l - 7) as u8);
+            ob.push(d as u8);
+        };
+        ht[hash(s & m) as usize] = i;
+        ht[hash(s >> 8) as usize] = i + 1;
+        i += 2;
+        a = i;
+    }
+    literals(b + 4 - a, a, ib, &mut ob);
+    ob.len() as u32
+}
+
 #[cfg(test)]
 mod tests {
+    use rand::RngCore;
+
     use super::*;
     use crate::primitives::bytes;
 
@@ -136,6 +243,15 @@ mod tests {
             let prev_len = len;
             len = flz_compress_len(&input);
             assert!(len > prev_len);
+        }
+    }
+
+    #[test]
+    fn test_flz_solady_parity() {
+        for _ in 0..1000 {
+            let mut input = [0; 4096];
+            rand::thread_rng().fill_bytes(&mut input);
+            assert_eq!(flz_compress_len(&input), solady_flz_compress(&input));
         }
     }
 }
