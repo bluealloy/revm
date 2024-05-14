76bff52cd10054ab9fd67b0a8c94df4bab0c6eb9
Brian Bland <brian.t.bland@gmail.com>
2024-05-13 17:37:08 -0700

    Add fastlz test

--- crates/revm/src/optimism/fast_lz.rs
+++ crates/revm/src/optimism/fast_lz.rs
@@ -1,6 +1,8 @@
 const HASH_LOG: u16 = 13;
 const MAX_L1_DISTANCE: u16 = 8192;
 
+/// Returns the length of the data after compression through FastLZ, based on
+// https://github.com/Vectorized/solady/blob/5315d937d79b335c668896d7533ac603adac5315/js/solady.js
 pub(crate) fn flz_compress_len(input: &[u8]) -> u32 {
     let mut idx: u32 = 0;
     let idx_limit: u32 = if input.len() < 13 {
@@ -13,7 +15,7 @@ pub(crate) fn flz_compress_len(input: &[u8]) -> u32 {
 
     idx += 2;
 
-    let mut length = 0;
+    let mut size = 0;
 
     let mut htab = [0; 8192];
 
@@ -43,27 +45,27 @@ pub(crate) fn flz_compress_len(input: &[u8]) -> u32 {
         idx -= 1;
 
         if idx > anchor {
-            length = literals(idx - anchor, length);
+            size = literals(idx - anchor, size);
         }
 
         let len = cmp(input, r + 3, idx + 3, idx_limit + 9);
-        length = flz_match(len, length);
+        size = flz_match(len, size);
 
         idx = set_next_hash(&mut htab, input, idx + len);
         idx = set_next_hash(&mut htab, input, idx);
         anchor = idx;
     }
 
-    literals(input.len() as u32 - anchor, length)
+    literals(input.len() as u32 - anchor, size)
 }
 
-fn literals(r: u32, length: u32) -> u32 {
-    let length = length + 0x21 * (r / 0x20);
+fn literals(r: u32, size: u32) -> u32 {
+    let size = size + 0x21 * (r / 0x20);
     let r = r % 0x20;
     if r != 0 {
-        length + r + 1
+        size + r + 1
     } else {
-        length
+        size
     }
 }
 
@@ -79,13 +81,13 @@ fn cmp(input: &[u8], p: u32, q: u32, r: u32) -> u32 {
     l
 }
 
-fn flz_match(l: u32, length: u32) -> u32 {
+fn flz_match(l: u32, size: u32) -> u32 {
     let l = l - 1;
-    let length = length + (3 * (l / 262));
+    let size = size + (3 * (l / 262));
     if l % 262 >= 6 {
-        length + 3
+        size + 3
     } else {
-        length + 2
+        size + 2
     }
 }
 
@@ -122,5 +124,18 @@ mod tests {
 
         let contract_call_tx_bytes = bytes!("02f901550a758302df1483be21b88304743f94f80e51afb613d764fa61751affd3313c190a86bb870151bd62fd12adb8e41ef24f3f000000000000000000000000000000000000000000000000000000000000006e000000000000000000000000af88d065e77c8cc2239327c5edb3a432268e5831000000000000000000000000000000000000000000000000000000000003c1e5000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000a000000000000000000000000000000000000000000000000000000000000000148c89ed219d02f1a5be012c689b4f5b731827bebe000000000000000000000000c001a033fd89cb37c31b2cba46b6466e040c61fc9b2a3675a7f5f493ebd5ad77c497f8a07cdf65680e238392693019b4092f610222e71b7cec06449cb922b93b6a12744e");
         assert_eq!(flz_compress_len(&contract_call_tx_bytes), 202);
+    }
+
+    #[test]
+    fn test_flz_compress_len_no_repeats() {
+        let mut input = Vec::new();
+        let mut len = 0;
+
+        for i in 0..256 {
+            input.push(i as u8);
+            let prev_len = len;
+            len = flz_compress_len(&input);
+            assert!(len > prev_len);
+        }
     }
 }
