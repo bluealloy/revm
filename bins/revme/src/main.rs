458500764c966544f7e352bddf76379e149afb61
rakita <dragan0rakita@gmail.com>
2024-05-24 18:29:17 +0200

    revert to previous println

--- bins/revme/src/main.rs
+++ bins/revme/src/main.rs
@@ -4,7 +4,7 @@ use structopt::StructOpt;
 pub fn main() -> Result<(), Error> {
     let cmd = MainCmd::from_args();
     if let Err(e) = cmd.run() {
-        println!("Error: {:?}", e);
+        println!("{:?}", e);
         return Err(e);
     }
     Ok(())
