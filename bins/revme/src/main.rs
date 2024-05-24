47b4f4dafc6798ebbf3dbad607ac70556fe66307
rakita <dragan0rakita@gmail.com>
2024-05-24 18:01:49 +0200

    return println and clippy

--- bins/revme/src/main.rs
+++ bins/revme/src/main.rs
@@ -4,7 +4,8 @@ use structopt::StructOpt;
 pub fn main() -> Result<(), Error> {
     let cmd = MainCmd::from_args();
     if let Err(e) = cmd.run() {
+        println!("Error: {:?}", e);
         return Err(e);
     }
-    return Ok(());
+    Ok(())
 }
