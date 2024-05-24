32feed0d0d928bd4f14a61c0ae4c3ab125782466
rakita <dragan0rakita@gmail.com>
2024-05-24 14:12:34 +0200

    chore: cargo update

--- crates/revm/src/db.rs
+++ crates/revm/src/db.rs
@@ -1,7 +1,7 @@
 //! [Database] implementations.
 
-#[cfg(feature = "alloydb")]
-pub mod alloydb;
+//#[cfg(feature = "alloydb")]
+//pub mod alloydb;
 pub mod emptydb;
 #[cfg(feature = "ethersdb")]
 pub mod ethersdb;
@@ -9,8 +9,8 @@ pub mod in_memory_db;
 pub mod states;
 
 pub use crate::primitives::db::*;
-#[cfg(feature = "alloydb")]
-pub use alloydb::AlloyDB;
+//#[cfg(feature = "alloydb")]
+//pub use alloydb::AlloyDB;
 pub use emptydb::{EmptyDB, EmptyDBTyped};
 #[cfg(feature = "ethersdb")]
 pub use ethersdb::EthersDB;
