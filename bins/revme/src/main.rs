28815b95553c72beae5c20de9c1b1d8a2174ef2b
rakita <dragan0rakita@gmail.com>
2024-05-24 15:41:23 +0200

    chore: skip tests with storage check and return error

--- bins/revme/src/main.rs
+++ bins/revme/src/main.rs
@@ -1,9 +1,10 @@
-use revme::cmd::MainCmd;
+use revme::cmd::{Error, MainCmd};
 use structopt::StructOpt;
 
-pub fn main() {
+pub fn main() -> Result<(), Error> {
     let cmd = MainCmd::from_args();
     if let Err(e) = cmd.run() {
-        println!("{}", e)
+        return Err(e);
     }
+    return Ok(());
 }
