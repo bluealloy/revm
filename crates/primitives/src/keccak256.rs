79d05d80534d50e9056071bdc2aa2dd614db86a9
Dmitry Savonin <nwtsoas@gmail.com>
2024-06-07 13:54:45 +0400

    changed fluentbase namespace and bindings names

--- crates/primitives/src/keccak256.rs
+++ crates/primitives/src/keccak256.rs
@@ -1,15 +1,15 @@
 use alloy_primitives::B256;
 
-#[link(wasm_import_module = "fluentbase_v1alpha")]
+#[link(wasm_import_module = "fluentbase_v1preview")]
 extern "C" {
-    fn _crypto_keccak256(data_offset: *const u8, data_len: u32, output32_offset: *mut u8);
+    fn _keccak256(data_offset: *const u8, data_len: u32, output32_offset: *mut u8);
 }
 
 #[inline(always)]
 pub fn keccak256(input: &[u8]) -> B256 {
     let mut result = B256::ZERO;
     unsafe {
-        _crypto_keccak256(input.as_ptr(), input.len() as u32, result.as_mut_ptr());
+        _keccak256(input.as_ptr(), input.len() as u32, result.as_mut_ptr());
     }
     result
 }
