3baf7714c1a0c35b4e8420dc8ea4b4c324219641
Wodann <Wodann@users.noreply.github.com>
2024-06-20 03:56:36 +0000

    refactor: rename gas_priority_fee to max_priority_fee_per_gas

--- crates/primitives/src/env.rs
+++ crates/primitives/src/env.rs
@@ -38,7 +38,7 @@ impl<ChainSpecT: ChainSpec> Env<ChainSpecT> {
     #[inline]
     pub fn effective_gas_price(&self) -> U256 {
         let gas_price = self.tx.gas_price();
-        if let Some(priority_fee) = self.tx.gas_priority_fee() {
+        if let Some(priority_fee) = self.tx.max_priority_fee_per_gas() {
             min(*gas_price, self.block.basefee() + priority_fee)
         } else {
             *gas_price
@@ -93,7 +93,7 @@ impl<ChainSpecT: ChainSpec> Env<ChainSpecT> {
     pub fn validate_tx<SPEC: Spec>(&self) -> Result<(), InvalidTransaction> {
         // BASEFEE tx check
         if SPEC::enabled(SpecId::LONDON) {
-            if let Some(priority_fee) = self.tx.gas_priority_fee() {
+            if let Some(priority_fee) = self.tx.max_priority_fee_per_gas() {
                 if priority_fee > self.tx.gas_price() {
                     // or gas_max_fee for eip1559
                     return Err(InvalidTransaction::PriorityFeeGreaterThanMaxFee);
@@ -618,7 +618,7 @@ impl Transaction for TxEnv {
     }
 
     #[inline]
-    fn gas_priority_fee(&self) -> Option<&U256> {
+    fn max_priority_fee_per_gas(&self) -> Option<&U256> {
         self.gas_priority_fee.as_ref()
     }
 
