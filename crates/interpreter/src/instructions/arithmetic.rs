530d0fc8128425c6625b3a3812bddd98310d5d9e
Thomas Coratger <60488569+tcoratger@users.noreply.github.com>
2024-07-18 00:06:33 +0200

    Update crates/interpreter/src/instructions/arithmetic.rs
    Co-authored-by: DaniPopes <57450786+DaniPopes@users.noreply.github.com>

--- crates/interpreter/src/instructions/arithmetic.rs
+++ crates/interpreter/src/instructions/arithmetic.rs
@@ -26,7 +26,7 @@ pub fn sub<H: Host + ?Sized>(interpreter: &mut Interpreter, _host: &mut H) {
 pub fn div<H: Host + ?Sized>(interpreter: &mut Interpreter, _host: &mut H) {
     gas!(interpreter, gas::LOW);
     pop_top!(interpreter, op1, op2);
-    if !(*op2).is_zero() {
+    if !op2.is_zero() {
         *op2 = op1.wrapping_div(*op2);
     }
 }
