f06a21e7452dd1643fa5e72fae842f3eae7b9e0f
Danno Ferrin <danno@numisight.com>
2024-08-02 15:57:49 -0600

    Add EOF Layout Fuzz Loop to `bytecode`
    Update `revme` so that when `bytecode` is called without arguments it goes into the standard

--- bins/revme/src/cmd/bytecode.rs
+++ bins/revme/src/cmd/bytecode.rs
@@ -1,16 +1,23 @@
+use std::io;
+
+use structopt::StructOpt;
+
 use revm::{
     interpreter::{analysis::validate_eof_inner, opcode::eof_printer::print_eof_code},
     primitives::{Bytes, Eof},
 };
-use structopt::StructOpt;
+use revm::interpreter::analysis::CodeType::ReturnOrStop;
+use revm::interpreter::analysis::EofError;
 
 /// Statetest command
 #[derive(StructOpt, Debug)]
 pub struct Cmd {
     /// Bytecode in hex format. If bytes start with 0xFE it will be interpreted as a EOF.
     /// Otherwise, it will be interpreted as a EOF bytecode.
-    #[structopt(required = true)]
+    /// If not provided, it will operate in interactive EOF validation mode.
+    #[structopt(default_value = "")]
     bytes: String,
+
 }
 
 impl Cmd {
@@ -23,7 +30,36 @@ impl Cmd {
         };
         let bytes: Bytes = bytes.into();
         if bytes.is_empty() {
-            eprintln!("Empty hex string");
+            loop {
+                let mut input = String::new();
+                io::stdin().read_line(&mut input).expect("Input Error");
+                if input.len() == 1 {
+                    // just a newline, so exit
+                    break;
+                }
+                let trimmed = input.trim().trim_start_matches("0x");
+                let Ok(bytes) = hex::decode(trimmed) else {
+                    println!("fail: Invalid hex string");
+                    return;
+                };
+                let bytes: Bytes = bytes.into();
+                match Eof::decode(bytes) {
+                    Ok(eof) => {
+                        match validate_eof_inner(&eof, Option::from(ReturnOrStop)) {
+                            Ok(_) => {
+                                println!("OK {}/{}/{}", eof.body.code_section.len(), eof.body.container_section.len(), eof.body.data_section.len());
+                            }
+                            Err(eof_error) => {
+                                match eof_error {
+                                    EofError::Decode(e) => println!("err decode: {}", e),
+                                    EofError::Validation(e) => println!("err validation: {}", e),
+                                }
+                            }
+                        }
+                    }
+                    Err(e) => println!("err: {:#?}", e),
+                }
+            }
             return;
         }
         if bytes[0] == 0xEF {
