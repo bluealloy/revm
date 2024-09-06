c60f5bbaf1a119d900cd315b97b0d007a094f3eb
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2024-09-06 16:22:08 +0200

    chore(revme): replace `structopt` with `clap`
    `structopt` has long been discontinued in favor of clap:
    <https://docs.rs/structopt/0.3.26/structopt/index.html#maintenance>

--- bins/revme/src/main.rs
+++ bins/revme/src/main.rs
@@ -1,11 +1,6 @@
+use clap::Parser;
 use revme::cmd::{Error, MainCmd};
-use structopt::StructOpt;
 
-pub fn main() -> Result<(), Error> {
-    let cmd = MainCmd::from_args();
-    if let Err(e) = cmd.run() {
-        println!("{:?}", e);
-        return Err(e);
-    }
-    Ok(())
+fn main() -> Result<(), Error> {
+    MainCmd::parse().run().inspect_err(|e| eprintln!("{e:?}"))
 }
