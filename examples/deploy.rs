56f5914885dd010499129ccd3525b2c746110fc6
clearloop <tianyi.gc@gmail.com>
2024-09-14 01:47:09 +0800

    feat(example): passing constructor parameters via creation code

--- examples/deploy.rs
+++ examples/deploy.rs
@@ -1,23 +1,23 @@
 use anyhow::{anyhow, bail};
 use revm::{
-    primitives::{bytes::Bytes, hex, EthereumWiring, ExecutionResult, Output, TxKind},
+    primitives::{bytes::Bytes, hex, EthereumWiring, ExecutionResult, Output, TxKind, U256},
     Evm, InMemoryDB,
 };
 
-/// [ PUSH1, 0x01, PUSH0, SSTORE ]
-const INIT_CODE: &[u8] = &[0x60, 0x01, 0x5f, 0x55];
+/// [ PUSH1, 0x01, PUSH1, 0x17, PUSH1, 0x1f, CODECOPY, PUSH0, MLOAD, PUSH0, SSTORE ]
+const INIT_CODE: &[u8] = &[
+    0x60, 0x01, 0x60, 0x17, 0x60, 0x1f, 0x39, 0x5f, 0x51, 0x5f, 0x55,
+];
 
-/// [ PUSH1, 0x02, PUSH1, 0x0e, PUSH0, CODECOPY, PUSH1, 0x02, PUSH0, RETURN]
-const RET: &[u8] = &[0x60, 0x02, 0x60, 0x0e, 0x5f, 0x39, 0x60, 0x02, 0x5f, 0xf3];
+/// [ PUSH1, 0x02, PUSH1, 0x15, PUSH0, CODECOPY, PUSH1, 0x02, PUSH0, RETURN]
+const RET: &[u8] = &[0x60, 0x02, 0x60, 0x15, 0x5f, 0x39, 0x60, 0x02, 0x5f, 0xf3];
 
 /// [ PUSH0 SLOAD ]
 const RUNTIME_BYTECODE: &[u8] = &[0x5f, 0x54];
 
-/// []
-const PARAMS: &[u8] = &hex!("");
-
 fn main() -> anyhow::Result<()> {
-    let bytecode: Bytes = [INIT_CODE, RET, RUNTIME_BYTECODE, PARAMS].concat().into();
+    let param = 0x42;
+    let bytecode: Bytes = [INIT_CODE, RET, RUNTIME_BYTECODE, &[param]].concat().into();
     let mut evm: Evm<'_, EthereumWiring<InMemoryDB, ()>> =
         Evm::<EthereumWiring<InMemoryDB, ()>>::builder()
             .with_default_db()
@@ -35,14 +35,14 @@ fn main() -> anyhow::Result<()> {
         ..
     } = ref_tx
     else {
-        bail!("Failed to create contract");
+        bail!("Failed to create contract: {ref_tx:#?}");
     };
 
     println!("Created contract at {address}");
     evm = evm
         .modify()
         .modify_tx_env(|tx| {
-            tx.transact_to = TxKind::Call(address.clone());
+            tx.transact_to = TxKind::Call(address);
             *tx.data = Default::default();
             tx.nonce += 1;
         })
@@ -54,12 +54,12 @@ fn main() -> anyhow::Result<()> {
         .get(&address)
         .ok_or_else(|| anyhow!("Contract not found"))?
         .storage
-        .get(&Default::default())
+        .get::<U256>(&Default::default())
     else {
-        bail!("Failed to write storage in the init code");
+        bail!("Failed to write storage in the init code: {result:#?}");
     };
 
     println!("storage U256(0) at{address}:  {storage0:#?}");
-    assert_eq!(storage0.present_value(), 1.try_into()?);
+    assert_eq!(storage0.present_value(), param.try_into()?, "{result:#?}");
     Ok(())
 }
