5b6846b538378d1fac58e666dd1c2f72424997af
James Prestwich <james@prestwi.ch>
2024-09-20 05:04:47 -0400

    chore: migrate off alloy-eips (#1789)

--- crates/specification/src/eip7702.rs
+++ crates/specification/src/eip7702.rs
@@ -6,4 +6,4 @@ pub use authorization_list::*;
 pub use constants::*;
 pub use recovered_authorization::*;
 
-pub use alloy_eips::eip7702::{Authorization, SignedAuthorization};
+pub use alloy_eip7702::{Authorization, SignedAuthorization};
