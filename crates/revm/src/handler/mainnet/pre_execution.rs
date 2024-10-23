b0a8b9d4ac92283f4066f40153c7716d3371ae76
rakita <dragan0rakita@gmail.com>
2024-10-23 13:12:12 +0200

    bump tests and fix auth chain_id check bug

--- crates/revm/src/handler/mainnet/pre_execution.rs
+++ crates/revm/src/handler/mainnet/pre_execution.rs
@@ -115,11 +115,15 @@ pub fn apply_eip7702_auth_list<SPEC: Spec, EXT, DB: Database>(
     let mut refunded_accounts = 0;
     for authorization in authorization_list.recovered_iter() {
         // 1. Verify the chain id is either 0 or the chain's current ID.
-        if !authorization.chain_id() == 0
+        //println!("Chain ID: {}", authorization.chain_id());
+        //println!("CFG Chain ID: {}", context.evm.inner.env.cfg.chain_id);
+        if authorization.chain_id() != 0
             && authorization.chain_id() != context.evm.inner.env.cfg.chain_id
         {
+            //println!("Skipped");
             continue;
         }
+        //println!("PASSED");
 
         // 2. Verify the `nonce` is less than `2**64 - 1`.
         if authorization.nonce() == u64::MAX {
