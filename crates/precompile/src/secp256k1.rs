2324c9ce75fb0c357efc89cd6bdc68785e72dec6
steven <corderosteven6@gmail.com>
2024-12-14 18:45:33 -0600

    chore: integrate rust-secp256k1

--- crates/precompile/src/secp256k1.rs
+++ crates/precompile/src/secp256k1.rs
@@ -45,20 +45,23 @@ mod secp256k1 {
 #[allow(clippy::module_inception)]
 mod secp256k1 {
     use primitives::{alloy_primitives::B512, keccak256, B256};
-    use secp256k1::{
+    use rust_secp256k1::{
         ecdsa::{RecoverableSignature, RecoveryId},
-        Message, SECP256K1,
+        Message, Secp256k1,
     };
 
     // Silence the unused crate dependency warning.
     use k256 as _;
+    use secp256k1 as _;
 
-    pub fn ecrecover(sig: &B512, recid: u8, msg: &B256) -> Result<B256, secp256k1::Error> {
+    pub fn ecrecover(sig: &B512, recid: u8, msg: &B256) -> Result<B256, rust_secp256k1::Error> {
+        let engine = Secp256k1::new();
+
         let recid = RecoveryId::from_i32(recid as i32).expect("recovery ID is valid");
         let sig = RecoverableSignature::from_compact(sig.as_slice(), recid)?;
 
-        let msg = Message::from_digest(msg.0);
-        let public = SECP256K1.recover_ecdsa(&msg, &sig)?;
+        let msg = Message::from_slice(msg.as_ref())?;
+        let public = engine.recover_ecdsa(&msg, &sig)?;
 
         let mut hash = keccak256(&public.serialize_uncompressed()[1..]);
         hash[..12].fill(0);
