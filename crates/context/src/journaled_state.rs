9b5a04b7bfd6d7e1ae0fe4639571a20a5d2be5bd
rakita <dragan0rakita@gmail.com>
2024-12-30 12:59:53 +0100

    rm debug print

--- crates/context/src/journaled_state.rs
+++ crates/context/src/journaled_state.rs
@@ -881,7 +881,6 @@ impl<DB: Database> JournaledState<DB> {
         key: U256,
         new: U256,
     ) -> Result<StateLoad<SStoreResult>, DB::Error> {
-        println!("sstore state: {:#?}", self.state);
         // assume that acc exists and load the slot.
         let present = self.sload(address, key)?;
         let acc = self.state.get_mut(&address).unwrap();
