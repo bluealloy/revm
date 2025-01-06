40818a854e538826f0319ebfa9773f395c25d1af
rakita <dragan0rakita@gmail.com>
2025-01-06 13:53:05 +0100

    fix(op): move l1block loading to verification

--- crates/revm/src/optimism.rs
+++ crates/revm/src/optimism.rs
@@ -7,9 +7,8 @@ mod l1block;
 mod precompile;
 
 pub use handler_register::{
-    clear, deduct_caller, end, last_frame_return, load_accounts, load_precompiles,
-    optimism_handle_register, output, refund, reimburse_caller, reward_beneficiary, validate_env,
-    validate_tx_against_state,
+    clear, deduct_caller, end, last_frame_return, load_precompiles, optimism_handle_register,
+    output, refund, reimburse_caller, reward_beneficiary, validate_env, validate_tx_against_state,
 };
 pub use l1block::{
     L1BlockInfo, BASE_FEE_RECIPIENT, L1_BLOCK_CONTRACT, L1_FEE_RECIPIENT, OPERATOR_FEE_RECIPIENT,
