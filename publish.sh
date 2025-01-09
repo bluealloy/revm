a2451cdb30bd9d9aaca95f13bd50e2eafb619d8f
rakita <rakita@users.noreply.github.com>
2025-01-09 01:44:54 +0100

    chore: align crates versions (#1983)

--- publish.sh
+++ publish.sh
@@ -4,9 +4,19 @@
 set -e
 
 cargo publish --package revm-primitives
-cargo publish --package revm-precompile
+cargo publish --package revm-specification
+cargo publish --package revm-bytecode
+cargo publish --package revm-state
+cargo publish --package revm-database-interface
+cargo publish --package revm-context-interface
+cargo publish --package revm-handler-interface
 cargo publish --package revm-interpreter
+cargo publish --package revm-precompile
+cargo publish --package revm-database
+cargo publish --package revm-context
+cargo publish --package revm-handler
 cargo publish --package revm
+cargo publish --package revm-inspector
+cargo publish --package revm-statetest-types
 cargo publish --package revme
-
-echo "All crates published"
+cargo publish --package revm-optimism
