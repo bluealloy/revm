f0d482e2b9908a6bfe9aa53bb20d82a92545f087
Ayush Dubey <61616662+Ayushdubey86@users.noreply.github.com>
2025-02-15 01:14:53 +0530

    indentation change

--- crates/precompile/src/bls12_381/reuse_const.rs
+++ crates/precompile/src/bls12_381/reuse_const.rs
@@ -1,16 +1,14 @@
 use crate::PrecompileWithAddress;
-
 use crate::bls12_381::g1_add;
 use crate::bls12_381::g1_msm;
 use crate::bls12_381::g2_add;
 use crate::bls12_381::g2_msm;
-use crate::bls12_381::pairing;
 use crate::bls12_381::map_fp_to_g1;
 use crate::bls12_381::map_fp2_to_g2;
+use crate::bls12_381::pairing;
 
 #[cfg(feature = "blst")]
 pub fn precompiles() -> impl Iterator<Item = PrecompileWithAddress> {
-
     [
         g1_add::PRECOMPILE,
         g1_msm::PRECOMPILE,
