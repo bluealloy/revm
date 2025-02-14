be13180aef691e209e2552a2f9f61f187e415d8c
Ayush Dubey <61616662+Ayushdubey86@users.noreply.github.com>
2025-02-15 01:03:03 +0530

    Add files via upload

---
+++ crates/precompile/src/bls12_381/reuse_const.rs
@@ -0,0 +1,24 @@
+use crate::PrecompileWithAddress;
+
+use crate::bls12_381::g1_add;
+use crate::bls12_381::g1_msm;
+use crate::bls12_381::g2_add;
+use crate::bls12_381::g2_msm;
+use crate::bls12_381::pairing;
+use crate::bls12_381::map_fp_to_g1;
+use crate::bls12_381::map_fp2_to_g2;
+
+#[cfg(feature = "blst")]
+pub fn precompiles() -> impl Iterator<Item = PrecompileWithAddress> {
+
+    [
+        g1_add::PRECOMPILE,
+        g1_msm::PRECOMPILE,
+        g2_add::PRECOMPILE,
+        g2_msm::PRECOMPILE,
+        pairing::PRECOMPILE,
+        map_fp_to_g1::PRECOMPILE,
+        map_fp2_to_g2::PRECOMPILE,
+    ]
+    .into_iter()
+}
