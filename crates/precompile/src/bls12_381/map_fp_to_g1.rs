b259b1e27248b84fc4f864dbed8a9db231cda1c4
Ayush Dubey <61616662+Ayushdubey86@users.noreply.github.com>
2025-02-18 10:54:36 +0530

    Update map_fp_to_g1.rs

--- crates/precompile/src/bls12_381/map_fp_to_g1.rs
+++ crates/precompile/src/bls12_381/map_fp_to_g1.rs
@@ -2,7 +2,7 @@ use super::{
     g1::encode_g1_point,
     utils::{fp_from_bendian, remove_padding},
 };
-use crate::bls12_381::bls12_381_const::{
+use crate::bls12_381_const::{
     MAP_FP_TO_G1_ADDRESS, MAP_FP_TO_G1_BASE_GAS_FEE, PADDED_FP_LENGTH,
 };
 use crate::{u64_to_address, PrecompileWithAddress};
