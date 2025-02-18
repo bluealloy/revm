f5e0d122e826f91aae78205fdbfd1bcd59c06b4e
Ayush Dubey <61616662+Ayushdubey86@users.noreply.github.com>
2025-02-18 10:57:59 +0530

    Update bls12_381.rs

--- crates/precompile/src/bls12_381.rs
+++ crates/precompile/src/bls12_381.rs
@@ -1,27 +1,18 @@
 use crate::PrecompileWithAddress;
 
 mod g1;
-#[cfg(feature = "blst")]
 pub mod g1_add;
-#[cfg(feature = "blst")]
 pub mod g1_msm;
 mod g2;
-#[cfg(feature = "blst")]
 pub mod g2_add;
-#[cfg(feature = "blst")]
 pub mod g2_msm;
-#[cfg(feature = "blst")]
 pub mod map_fp2_to_g2;
-#[cfg(feature = "blst")]
 pub mod map_fp_to_g1;
-#[cfg(feature = "blst")]
 pub mod pairing;
 mod utils;
-pub mod bls12_381_const;
 pub mod msm;
 
 /// Returns the BLS12-381 precompiles with their addresses.
-#[cfg(feature = "blst")]
 pub fn precompiles() -> impl Iterator<Item = PrecompileWithAddress> {
     [
         g1_add::PRECOMPILE,
