fd8d4f89be7aa246c105273da3eb968508cede02
startup-dreamer <prsumit35@gmail.com>
2025-03-10 17:27:34 +0530

    chore: removed the deprecated libsecp256k1 in favour of k256

--- crates/precompile/src/secp256k1.rs
+++ crates/precompile/src/secp256k1.rs
@@ -1,8 +1,6 @@
 #[cfg(feature = "secp256k1")]
 pub mod bitcoin_secp256k1;
 pub mod k256;
-#[cfg(feature = "libsecp256k1")]
-pub mod parity_libsecp256k1;
 
 use crate::{
     utilities::right_pad, PrecompileError, PrecompileOutput, PrecompileResult,
@@ -40,8 +38,6 @@ pub fn ec_recover_run(input: &Bytes, gas_limit: u64) -> PrecompileResult {
 cfg_if::cfg_if! {
     if #[cfg(feature = "secp256k1")] {
         pub use bitcoin_secp256k1::ecrecover;
-    } else if #[cfg(feature = "libsecp256k1")] {
-        pub use parity_libsecp256k1::ecrecover;
     } else {
         pub use k256::ecrecover;
     }
