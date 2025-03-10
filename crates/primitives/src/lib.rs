60fdb4f0889ef985cfa6424a07b7f9dd109927a1
rakita <dragan0rakita@gmail.com>
2025-03-10 12:48:39 +0100

    no std

--- crates/primitives/src/lib.rs
+++ crates/primitives/src/lib.rs
@@ -4,6 +4,10 @@
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
 #![cfg_attr(not(feature = "std"), no_std)]
 
+#[macro_use]
+#[cfg(not(feature = "std"))]
+extern crate alloc as std;
+
 pub mod constants;
 pub mod eip170;
 pub mod eip4844;
