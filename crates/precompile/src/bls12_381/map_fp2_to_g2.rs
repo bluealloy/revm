e06e9df34efaf6f51c88032e4c128d376c49a01c
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2025-03-16 21:31:18 +0000

    map_fp2_to_g2:
    - use safe blst wrapper

--- crates/precompile/src/bls12_381/map_fp2_to_g2.rs
+++ crates/precompile/src/bls12_381/map_fp2_to_g2.rs
@@ -1,10 +1,12 @@
-use super::{g2::check_canonical_fp2, g2::encode_g2_point, utils::remove_padding};
+use super::{
+    blst::map_fp2_to_g2 as blst_map_fp2_to_g2, g2::check_canonical_fp2, g2::encode_g2_point,
+    utils::remove_padding,
+};
 use crate::bls12_381_const::{
     MAP_FP2_TO_G2_ADDRESS, MAP_FP2_TO_G2_BASE_GAS_FEE, PADDED_FP2_LENGTH, PADDED_FP_LENGTH,
 };
 use crate::{u64_to_address, PrecompileWithAddress};
 use crate::{PrecompileError, PrecompileOutput, PrecompileResult};
-use blst::{blst_map_to_g2, blst_p2, blst_p2_affine, blst_p2_to_affine};
 use primitives::Bytes;
 
 /// [EIP-2537](https://eips.ethereum.org/EIPS/eip-2537#specification) BLS12_MAP_FP2_TO_G2 precompile.
@@ -30,15 +32,7 @@ pub(super) fn map_fp2_to_g2(input: &Bytes, gas_limit: u64) -> PrecompileResult {
     let input_p0_x = remove_padding(&input[..PADDED_FP_LENGTH])?;
     let input_p0_y = remove_padding(&input[PADDED_FP_LENGTH..PADDED_FP2_LENGTH])?;
     let fp2 = check_canonical_fp2(input_p0_x, input_p0_y)?;
-
-    let mut p = blst_p2::default();
-    // SAFETY: `p` and `fp2` are blst values.
-    // Third argument is unused if null.
-    unsafe { blst_map_to_g2(&mut p, &fp2, core::ptr::null()) };
-
-    let mut p_aff = blst_p2_affine::default();
-    // SAFETY: `p_aff` and `p` are blst values.
-    unsafe { blst_p2_to_affine(&mut p_aff, &p) };
+    let p_aff = blst_map_fp2_to_g2(&fp2);
 
     let out = encode_g2_point(&p_aff);
     Ok(PrecompileOutput::new(MAP_FP2_TO_G2_BASE_GAS_FEE, out))
