d99f1bda892eb886cdaa956e972e63082ecfea9c
Emilia Hane <elsaemiliaevahane@gmail.com>
2025-03-21 10:51:34 -0100

    chore(op): Remove redundant trait DepositTransaction (#2265)

--- crates/optimism/src/transaction/deposit.rs
+++ crates/optimism/src/transaction/deposit.rs
@@ -2,18 +2,6 @@ use revm::primitives::B256;
 
 pub const DEPOSIT_TRANSACTION_TYPE: u8 = 0x7E;
 
-/// Deposit transaction trait
-pub trait DepositTransaction {
-    /// Source hash of the deposit transaction
-    fn source_hash(&self) -> B256;
-
-    /// Mint of the deposit transaction
-    fn mint(&self) -> Option<u128>;
-
-    /// Whether the transaction is a system transaction
-    fn is_system_transaction(&self) -> bool;
-}
-
 #[derive(Clone, Debug, Default, PartialEq, Eq)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct DepositTransactionParts {
