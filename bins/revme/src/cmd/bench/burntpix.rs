6f383227cad2c0eb4949533a2e27de015ca6bdb9
nemnem7 <115541090+nemnem7@users.noreply.github.com>
2025-03-26 00:00:11 +0100

    feat: Add criterion to revme bench command (#2295)
    * temp code preview
    
    * add criterion to benchmarks
    
    * added args to fn run() calls in bins/revme/
    
    * modifed the code inside the benchmark tests
    
    * left only .replay function inside bench_functions and added minor revisions to the rest of the code
    
    * edited comment annotation in burntpix.rs file
    
    * added comments and set different default value for warm_up_time

--- bins/revme/src/cmd/bench/burntpix.rs
+++ bins/revme/src/cmd/bench/burntpix.rs
@@ -1,5 +1,6 @@
 pub mod static_data;
 
+use criterion::Criterion;
 use static_data::{
     BURNTPIX_ADDRESS_ONE, BURNTPIX_ADDRESS_THREE, BURNTPIX_ADDRESS_TWO, BURNTPIX_BYTECODE_FOUR,
     BURNTPIX_BYTECODE_ONE, BURNTPIX_BYTECODE_THREE, BURNTPIX_BYTECODE_TWO, BURNTPIX_MAIN_ADDRESS,
@@ -9,17 +10,15 @@ use static_data::{
 use alloy_sol_types::{sol, SolCall};
 use database::{CacheDB, BENCH_CALLER};
 use revm::{
-    context_interface::result::{ExecutionResult, Output},
     database_interface::EmptyDB,
     primitives::{hex, keccak256, Address, Bytes, TxKind, B256, U256},
     state::{AccountInfo, Bytecode},
     Context, ExecuteEvm, MainBuilder, MainContext,
 };
 
-use std::fs::File;
-use std::{error::Error, time::Instant};
+use std::error::Error;
 
-use std::{io::Write, str::FromStr};
+use std::str::FromStr;
 
 sol! {
     #[derive(Debug, PartialEq, Eq)]
@@ -28,7 +27,7 @@ sol! {
     }
 }
 
-pub fn run() {
+pub fn run(criterion: &mut Criterion) {
     let (seed, iterations) = try_init_env_vars().expect("Failed to parse env vars");
 
     let run_call_data = IBURNTPIX::runCall { seed, iterations }.abi_encode();
@@ -45,46 +44,52 @@ pub fn run() {
         })
         .build_mainnet();
 
-    let started = Instant::now();
-    let tx_result = evm.replay().unwrap().result;
-    let return_data = match tx_result {
-        ExecutionResult::Success {
-            output, gas_used, ..
-        } => {
-            println!("Gas used: {:?}", gas_used);
-            println!("Time elapsed: {:?}", started.elapsed());
-            match output {
-                Output::Call(value) => value,
-                _ => unreachable!("Unexpected output type"),
-            }
-        }
-        _ => unreachable!("Execution failed: {:?}", tx_result),
-    };
+    criterion.bench_function("burntpix", |b| {
+        b.iter(|| {
+            let _ = evm.replay();
+        })
+    });
 
-    // Remove returndata offset and length from output
-    let returndata_offset = 64;
-    let data = &return_data[returndata_offset..];
+    // Collects the data and uses it to generate the svg after running the benchmark
+    //     let return_data = match tx_result {
+    //         ExecutionResult::Success {
+    //             output, gas_used, ..
+    //         } => {
+    //             println!("Gas used: {:?}", gas_used);
+    //             match output {
+    //                 Output::Call(value) => value,
+    //                 _ => unreachable!("Unexpected output type"),
+    //             }
+    //         }
+    //         _ => unreachable!("Execution failed: {:?}", tx_result),
+    //     };
 
-    // Remove trailing zeros
-    let trimmed_data = data
-        .split_at(data.len() - data.iter().rev().filter(|&x| *x == 0).count())
-        .0;
-    let file_name = format!("{}_{}", seed, iterations);
+    //     // Remove returndata offset and length from output
+    //     let returndata_offset = 64;
+    //     let data = &return_data[returndata_offset..];
 
-    svg(file_name, trimmed_data).expect("Failed to store svg");
+    //     // Remove trailing zeros
+    //     let trimmed_data = data
+    //         .split_at(data.len() - data.iter().rev().filter(|&x| *x == 0).count())
+    //         .0;
+    //     let file_name = format!("{}_{}", seed, iterations);
+
+    //     svg(file_name, trimmed_data).expect("Failed to store svg");
+    // });
 }
 
-fn svg(filename: String, svg_data: &[u8]) -> Result<(), Box<dyn Error>> {
-    let current_dir = std::env::current_dir()?;
-    let svg_dir = current_dir.join("burntpix").join("svgs");
-    std::fs::create_dir_all(&svg_dir)?;
+// Actually generates the svg
+// fn svg(filename: String, svg_data: &[u8]) -> Result<(), Box<dyn Error>> {
+//     let current_dir = std::env::current_dir()?;
+//     let svg_dir = current_dir.join("burntpix").join("svgs");
+//     std::fs::create_dir_all(&svg_dir)?;
 
-    let file_path = svg_dir.join(format!("{}.svg", filename));
-    let mut file = File::create(file_path)?;
-    file.write_all(svg_data)?;
+//     let file_path = svg_dir.join(format!("{}.svg", filename));
+//     let mut file = File::create(file_path)?;
+//     file.write_all(svg_data)?;
 
-    Ok(())
-}
+//     Ok(())
+// }
 
 const DEFAULT_SEED: &str = "0";
 const DEFAULT_ITERATIONS: &str = "0x7A120";
--- bins/revme/src/cmd/bench/transfer.rs
+++ bins/revme/src/cmd/bench/transfer.rs
@@ -1,13 +1,12 @@
+use criterion::Criterion;
 use database::{BenchmarkDB, BENCH_CALLER, BENCH_TARGET};
 use revm::{
     bytecode::Bytecode,
     primitives::{TxKind, U256},
     Context, ExecuteEvm, MainBuilder, MainContext,
 };
-use std::time::Instant;
 
-pub fn run() {
-    let time = Instant::now();
+pub fn run(criterion: &mut Criterion) {
     let mut evm = Context::mainnet()
         .with_db(BenchmarkDB::new_bytecode(Bytecode::new()))
         .modify_tx_chained(|tx| {
@@ -17,13 +16,9 @@ pub fn run() {
             tx.value = U256::from(10);
         })
         .build_mainnet();
-    println!("Init: {:?}", time.elapsed());
-
-    let time = Instant::now();
-    let _ = evm.replay();
-    println!("First run: {:?}", time.elapsed());
-
-    let time = Instant::now();
-    let _ = evm.replay();
-    println!("Second run: {:?}", time.elapsed());
+    criterion.bench_function("transfer", |b| {
+        b.iter(|| {
+            let _ = evm.replay();
+        })
+    });
 }
