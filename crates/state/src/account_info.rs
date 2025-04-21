ce9be1ffa17d394397f58d0c693f8b36016b3fc7
Shane K Moore <41407272+shane-moore@users.noreply.github.com>
2025-04-20 21:04:41 -0400

    chore: add AccountInfo setters (#2429)
    * chore: add AccountInfo setters
    
    * chore: updates per pr review
    
    * Update crates/state/src/account_info.rs

--- crates/state/src/account_info.rs
+++ crates/state/src/account_info.rs
@@ -117,6 +117,53 @@ impl AccountInfo {
         self
     }
 
+    /// Sets the [`AccountInfo`] `balance`.
+    #[inline]
+    pub fn set_balance(&mut self, balance: U256) -> &mut Self {
+        self.balance = balance;
+        self
+    }
+
+    /// Sets the [`AccountInfo`] `nonce`.
+    #[inline]
+    pub fn set_nonce(&mut self, nonce: u64) -> &mut Self {
+        self.nonce = nonce;
+        self
+    }
+
+    /// Sets the [`AccountInfo`] `code_hash` and clears any cached bytecode.
+    ///
+    /// # Note
+    ///
+    /// Calling this after `set_code(...)` will remove the bytecode you just set.
+    /// If you intend to mutate the code, use only `set_code`.
+    #[inline]
+    pub fn set_code_hash(&mut self, code_hash: B256) -> &mut Self {
+        self.code = None;
+        self.code_hash = code_hash;
+        self
+    }
+
+    /// Replaces the [`AccountInfo`] bytecode and recalculates `code_hash`.
+    ///
+    /// # Note
+    ///
+    /// As code hash is calculated with [`Bytecode::hash_slow`] there will be performance penalty if used frequently.
+    #[inline]
+    pub fn set_code(&mut self, code: Bytecode) -> &mut Self {
+        self.code_hash = code.hash_slow();
+        self.code = Some(code);
+        self
+    }
+    /// Sets the bytecode and its hash.
+    ///
+    /// # Note
+    ///
+    /// It is on the caller's responsibility to ensure that the bytecode hash is correct.
+    pub fn set_code_and_hash(&mut self, code: Bytecode, code_hash: B256) {
+        self.code_hash = code_hash;
+        self.code = Some(code);
+    }
     /// Returns a copy of this account with the [`Bytecode`] removed.
     ///
     /// This is useful when creating journals or snapshots of the state, where it is
