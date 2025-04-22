2e58f1f98a60e049dcac98217fe9eaa69931ec2a
Pana <pana.wang@outlook.com>
2025-04-22 20:17:15 +0800

    chore: bump state tests version to 4.3 (#2434)

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Version for the execution spec tests
-VERSION="v4.2.0"
+VERSION="v4.3.0"
 # Version for the EOF spec tests, it is currently upgrading to eof devnet-1 so we will use devnet-0 suite.
 EOF_VERSION="eip7692@v2.3.0"
 
