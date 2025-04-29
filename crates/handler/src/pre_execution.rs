38067daae362a6f8e02ab69d92e7f8cb2f40f498
Spencer Solit <sol@seismic.systems>
2025-04-29 12:21:29 -0400

    minor fix

--- crates/handler/src/pre_execution.rs
+++ crates/handler/src/pre_execution.rs
@@ -12,7 +12,7 @@ use context_interface::{
     transaction::{Transaction, TransactionType},
     Block, Cfg, Database,
 };
-use primitives::StorageValue;
+use primitives::StorageKey;
 use core::cmp::Ordering;
 use primitives::{eip7702, hardfork::SpecId, KECCAK_EMPTY, U256};
 use state::AccountInfo;
@@ -61,7 +61,7 @@ pub fn load_accounts<
                 } else {
                     journal.warm_account_and_storage(
                         *address,
-                        storage.map(|i| StorageValue::from_be_bytes(i.0)),
+                        storage.map(|i| StorageKey::from_be_bytes(i.0)),
                     )?;
                 }
             }
