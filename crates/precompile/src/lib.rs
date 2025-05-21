32eb06281c2ee1122f00f1b465cd411697d2aa26
rakita <rakita@users.noreply.github.com>
2025-05-21 13:22:57 +0200

    feat(Osaka): modexp input limit and gas change, EIP-7823 and EIP-7883 (#2531)
    * wip modexp changes for osaka
    
    * add Osaka precompile
    
    * add tests
    
    * typos

--- crates/precompile/src/lib.rs
+++ crates/precompile/src/lib.rs
@@ -80,6 +80,7 @@ impl Precompiles {
             PrecompileSpecId::BERLIN => Self::berlin(),
             PrecompileSpecId::CANCUN => Self::cancun(),
             PrecompileSpecId::PRAGUE => Self::prague(),
+            PrecompileSpecId::OSAKA => Self::osaka(),
         }
     }
 
@@ -188,9 +189,19 @@ impl Precompiles {
         })
     }
 
+    /// Returns precompiles for Osaka spec.
+    pub fn osaka() -> &'static Self {
+        static INSTANCE: OnceBox<Precompiles> = OnceBox::new();
+        INSTANCE.get_or_init(|| {
+            let mut precompiles = Self::prague().clone();
+            precompiles.extend([modexp::OSAKA]);
+            Box::new(precompiles)
+        })
+    }
+
     /// Returns the precompiles for the latest spec.
     pub fn latest() -> &'static Self {
-        Self::prague()
+        Self::osaka()
     }
 
     /// Returns an iterator over the precompiles addresses.
@@ -343,6 +354,10 @@ pub enum PrecompileSpecId {
     /// * `BLS12_MAP_FP_TO_G1` at address 0x10
     /// * `BLS12_MAP_FP2_TO_G2` at address 0x11
     PRAGUE,
+    /// Osaka spec added changes to modexp precompile:
+    /// * [`EIP-7823: Set upper bounds for MODEXP`](https://eips.ethereum.org/EIPS/eip-7823).
+    /// * [`EIP-7883: ModExp Gas Cost Increase`](https://eips.ethereum.org/EIPS/eip-7883)
+    OSAKA,
 }
 
 impl From<SpecId> for PrecompileSpecId {
@@ -363,7 +378,8 @@ impl PrecompileSpecId {
             ISTANBUL | MUIR_GLACIER => Self::ISTANBUL,
             BERLIN | LONDON | ARROW_GLACIER | GRAY_GLACIER | MERGE | SHANGHAI => Self::BERLIN,
             CANCUN => Self::CANCUN,
-            PRAGUE | OSAKA => Self::PRAGUE,
+            PRAGUE => Self::PRAGUE,
+            OSAKA => Self::OSAKA,
         }
     }
 }
