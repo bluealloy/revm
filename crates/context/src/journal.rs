9ff18d048ce1ed4e763d0a3cf6484e9821d9ee7b
rakita <dragan0rakita@gmail.com>
2025-05-31 14:48:49 +0200

    refactor: unify calling of journal account loading

--- crates/context/src/journal.rs
+++ crates/context/src/journal.rs
@@ -167,7 +167,7 @@ impl<DB: Database, ENTRY: JournalEntryTr> JournalTr for Journal<DB, ENTRY> {
         storage_keys: impl IntoIterator<Item = StorageKey>,
     ) -> Result<(), <Self::Database as Database>::Error> {
         self.inner
-            .initial_account_load(&mut self.database, address, storage_keys)?;
+            .load_account_optional(&mut self.database, address, false, storage_keys)?;
         Ok(())
     }
 
