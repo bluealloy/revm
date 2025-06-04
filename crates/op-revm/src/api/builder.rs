13de1d4acd8d21b97702ea5e2b4b596d5073d0f3
0xDmtri <0xDmtri@protonmail.com>
2025-06-04 12:13:45 +0200

    chore(op-revm): impl type alias for Default OpEvm (#2576)

--- crates/op-revm/src/api/builder.rs
+++ crates/op-revm/src/api/builder.rs
@@ -1,5 +1,5 @@
 //! Optimism builder trait [`OpBuilder`] used to build [`OpEvm`].
-use crate::{evm::OpEvm, transaction::OpTxTr, L1BlockInfo, OpSpecId};
+use crate::{evm::OpEvm, precompiles::OpPrecompiles, transaction::OpTxTr, L1BlockInfo, OpSpecId};
 use revm::{
     context::Cfg,
     context_interface::{Block, JournalTr},
@@ -9,19 +9,20 @@ use revm::{
     Context, Database,
 };
 
+/// Type alias for default OpEvm
+pub type DefaultOpEvm<CTX, INSP = ()> =
+    OpEvm<CTX, INSP, EthInstructions<EthInterpreter, CTX>, OpPrecompiles>;
+
 /// Trait that allows for optimism OpEvm to be built.
 pub trait OpBuilder: Sized {
     /// Type of the context.
     type Context;
 
     /// Build the op.
-    fn build_op(self) -> OpEvm<Self::Context, (), EthInstructions<EthInterpreter, Self::Context>>;
+    fn build_op(self) -> DefaultOpEvm<Self::Context>;
 
     /// Build the op with an inspector.
-    fn build_op_with_inspector<INSP>(
-        self,
-        inspector: INSP,
-    ) -> OpEvm<Self::Context, INSP, EthInstructions<EthInterpreter, Self::Context>>;
+    fn build_op_with_inspector<INSP>(self, inspector: INSP) -> DefaultOpEvm<Self::Context, INSP>;
 }
 
 impl<BLOCK, TX, CFG, DB, JOURNAL> OpBuilder for Context<BLOCK, TX, CFG, DB, JOURNAL, L1BlockInfo>
@@ -34,14 +35,11 @@ where
 {
     type Context = Self;
 
-    fn build_op(self) -> OpEvm<Self::Context, (), EthInstructions<EthInterpreter, Self::Context>> {
+    fn build_op(self) -> DefaultOpEvm<Self::Context> {
         OpEvm::new(self, ())
     }
 
-    fn build_op_with_inspector<INSP>(
-        self,
-        inspector: INSP,
-    ) -> OpEvm<Self::Context, INSP, EthInstructions<EthInterpreter, Self::Context>> {
+    fn build_op_with_inspector<INSP>(self, inspector: INSP) -> DefaultOpEvm<Self::Context, INSP> {
         OpEvm::new(self, inspector)
     }
 }
