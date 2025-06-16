ba3a5bf71eb3072ee16a2f85ad564fab9b81460f
bfday <stanislav@fluentlabs.xyz>
2025-06-16 15:50:34 +0700

    feat(svm): cover extended code size with feature; rollback some comments; get rid of copy-paste in svm frame processing

--- crates/primitives/src/eip170.rs
+++ crates/primitives/src/eip170.rs
@@ -3,4 +3,7 @@
 /// EIP-170: Contract code size limit
 ///
 /// By default the limit is `0x6000` (~25kb).
+#[cfg(not(feature = "extended_code_size"))]
+pub const MAX_CODE_SIZE: usize = 0x6000;
+#[cfg(feature = "extended_code_size")]
 pub const MAX_CODE_SIZE: usize = 0x60000;
