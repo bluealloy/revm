2e9c3777378756aa067bc0a700557a087f6ed955
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2025-07-21 19:47:31 +0100

    move doc comment to crypto specific module

--- crates/precompile/src/crypto/secp256k1/mod.rs
+++ crates/precompile/src/crypto/secp256k1/mod.rs
@@ -1,4 +1,12 @@
 //! secp256k1 cryptographic implementations
+//!
+//! Depending on enabled features, it will use different implementations of `ecrecover`:
+//! * [`k256`](https://crates.io/crates/k256) - uses maintained pure rust lib `k256`, it is perfect use for no_std environments.
+//! * [`secp256k1`](https://crates.io/crates/secp256k1) - uses `bitcoin_secp256k1` lib, it is a C implementation of secp256k1 used in bitcoin core.
+//!   It is faster than k256 and enabled by default and in std environment.
+//! * [`libsecp256k1`](https://crates.io/crates/libsecp256k1) - is made from parity in pure rust, it is alternative for k256.
+//!
+//! Order of preference is `secp256k1` -> `k256` -> `libsecp256k1`. Where if no features are enabled, it will use `k256`.
 
 pub mod constants;
 
--- crates/precompile/src/secp256k1.rs
+++ crates/precompile/src/secp256k1.rs
@@ -1,12 +1,7 @@
 //! `ecrecover` precompile.
 //!
-//! Depending on enabled features, it will use different implementations of `ecrecover`.
-//! * [`k256`](https://crates.io/crates/k256) - uses maintained pure rust lib `k256`, it is perfect use for no_std environments.
-//! * [`secp256k1`](https://crates.io/crates/secp256k1) - uses `bitcoin_secp256k1` lib, it is a C implementation of secp256k1 used in bitcoin core.
-//!   It is faster than k256 and enabled by default and in std environment.
-//! * [`libsecp256k1`](https://crates.io/crates/libsecp256k1) - is made from parity in pure rust, it is alternative for k256.
-//!
-//! Order of preference is `secp256k1` -> `k256` -> `libsecp256k1`. Where if no features are enabled, it will use `k256`.
+//! The implementation uses the `secp256k1` curve to recover the public key from a signature.
+//! See [`crypto::secp256k1`](crate::crypto::secp256k1) for the underlying implementations.
 //!
 //! Input format:
 //! [32 bytes for message][64 bytes for signature][1 byte for recovery id]
