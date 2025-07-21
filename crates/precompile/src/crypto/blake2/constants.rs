acbbc5bdd46d6beefe537f16895111b4664c9046
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2025-07-21 20:11:03 +0100

    remove constants file

--- crates/precompile/src/crypto/blake2/constants.rs
+++
@@ -1,7 +0,0 @@
-//! Constants for Blake2 compression
-
-/// Length of the Blake2 state vector
-pub const STATE_LENGTH: usize = 8;
-
-/// Length of the Blake2 message block in bytes
-pub const MESSAGE_LENGTH: usize = 128;
