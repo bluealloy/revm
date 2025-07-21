6b6838e04509aff5bc41b8491b32d285d6146860
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2025-07-21 19:02:43 +0100

    add inline(always) for hash functions

--- crates/precompile/src/crypto/hash/mod.rs
+++ crates/precompile/src/crypto/hash/mod.rs
@@ -5,12 +5,14 @@ pub mod constants;
 use sha2::Digest;
 
 /// Compute SHA-256 hash
+#[inline(always)]
 pub fn sha256(input: &[u8]) -> [u8; constants::SHA256_LENGTH] {
     let output = sha2::Sha256::digest(input);
     output.into()
 }
 
 /// Compute RIPEMD-160 hash (padded to 32 bytes)
+#[inline(always)]
 pub fn ripemd160(input: &[u8]) -> [u8; 32] {
     let mut hasher = ripemd::Ripemd160::new();
     hasher.update(input);
