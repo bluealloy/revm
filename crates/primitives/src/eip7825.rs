5e5ff2a77655e870c172bfd521025fd011dae339
Roman Krasiuk <rokrassyuk@gmail.com>
2025-07-22 13:13:53 +0200

    feat(osaka): update EIP-7825 constant

--- crates/primitives/src/eip7825.rs
+++ crates/primitives/src/eip7825.rs
@@ -1,12 +1,11 @@
 //! EIP-7825: Transaction Gas Limit Cap  
-//! Introduce a protocol-level cap on the maximum gas used by a transaction to 30 million.
+//! Introduce a protocol-level cap on the maximum gas used by a transaction to 16 777 216.
 
 /// Transaction gas limit cap.
 ///
 /// # Rationale from EIP
 ///
-/// The proposed cap of 30 million gas is based on the typical size of Ethereum blocks today,
-/// which often range between 30-40 million gas. This value is large enough to allow complex
-/// transactions, such as contract deployments and advanced DeFi interactions, while still
-/// reserving space for other transactions within a block.
-pub const TX_GAS_LIMIT_CAP: u64 = 30_000_000;
+/// The proposed cap of 16,777,216 gas (2^24) provides a clean power-of-two boundary that simplifies implementation while still
+/// being large enough to accommodate most complex transactions, including contract deployments and advanced DeFi interactions.
+/// This value represents approximately half of typical block sizes (30-40 million gas), ensuring multiple transactions can fit within each block.
+pub const TX_GAS_LIMIT_CAP: u64 = 16_777_216;
