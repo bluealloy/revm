4ebf182a7328fd69f81ac83b3b35a03ca03087eb
rakita <dragan0rakita@gmail.com>
2025-07-31 23:42:16 +0200

    nit cleanup

--- crates/precompile/src/kzg_point_evaluation/blst.rs
+++ crates/precompile/src/kzg_point_evaluation/blst.rs
@@ -9,7 +9,6 @@ use ::blst::{
     blst_p1_affine, blst_p1_affine_in_g1, blst_p1_affine_on_curve, blst_p2_affine, blst_scalar,
     blst_scalar_fr_check, blst_scalar_from_bendian,
 };
-//use blst::{blst_fp12, blst_fp12_is_one, blst_miller_loop, blst_scalar_fr_check};
 use std::string::ToString;
 
 /// Verify KZG proof using BLST BLS12-381 implementation.
