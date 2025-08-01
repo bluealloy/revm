f2c990496872f557289b42c13d33d5a40899aafb
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2025-08-01 22:18:36 +0200

    try removing tails again for perf

--- crates/interpreter/src/instructions.rs
+++ crates/interpreter/src/instructions.rs
@@ -159,7 +159,8 @@ mod tail {
                         table[bytecode::opcode::$instr as usize] = conv(self::$instr);
                     )*
                     table
-                }
+                };
+                super::cx::table::<W, H>()
             }
         };
     }
