c4bee08967e70992285fb3c3727004aa7eddf930
rakita <dragan0rakita@gmail.com>
2025-08-12 00:24:08 +0200

    use caller id for reimbursment

--- crates/handler/src/post_execution.rs
+++ crates/handler/src/post_execution.rs
@@ -1,4 +1,5 @@
 use crate::FrameResult;
+use context::journal;
 use context_interface::{
     journaled_state::JournalTr,
     result::{ExecutionResult, HaltReasonTr},
@@ -36,12 +37,12 @@ pub fn reimburse_caller<CTX: ContextTr>(
 ) -> Result<(), <CTX::Db as Database>::Error> {
     let basefee = context.block().basefee() as u128;
     // TODO save AccountId somewhere so we can access it here.
-    let caller = context.tx().caller().into();
+    let caller = context.journal().caller_address_id().unwrap();
     let effective_gas_price = context.tx().effective_gas_price(basefee);
 
     // Return balance of not spend gas.
     context.journal_mut().balance_incr(
-        caller,
+        caller.to_id(),
         U256::from(
             effective_gas_price.saturating_mul((gas.remaining() + gas.refunded() as u64) as u128),
         ) + additional_refund,
