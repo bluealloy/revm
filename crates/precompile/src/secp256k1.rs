a04f1fba04c335c7171109503e4b321ecddc3fdf
oooLowNeoNooo <ooolowneonooo@gmail.com>
2025-08-28 12:38:58 +0200

    Update secp256k1.rs

--- crates/precompile/src/secp256k1.rs
+++ crates/precompile/src/secp256k1.rs
@@ -19,6 +19,9 @@ pub mod k256;
 #[cfg(feature = "libsecp256k1")]
 pub mod parity_libsecp256k1;
 
+/// Cryptographic operations for secp256k1
+pub mod crypto;
+
 use crate::{
     crypto, utilities::right_pad, Precompile, PrecompileError, PrecompileId, PrecompileOutput,
     PrecompileResult,
