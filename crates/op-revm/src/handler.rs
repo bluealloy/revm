5e95c440136461c3aa3b6d08df42349ce0adb45f
rakita <dragan0rakita@gmail.com>
2025-10-10 11:44:02 +0200

    chore: bump eest tests v5.3.0

--- crates/op-revm/src/handler.rs
+++ crates/op-revm/src/handler.rs
@@ -146,7 +146,7 @@ where
 
         // L1 block info is stored in the context for later use.
         // and it will be reloaded from the database if it is not for the current block.
-        if ctx.chain().l2_block != block_number {
+        if ctx.chain().l2_block != block_number || block_number == 0 {
             *ctx.chain_mut() = L1BlockInfo::try_fetch(ctx.db_mut(), block_number, spec)?;
         }
 
