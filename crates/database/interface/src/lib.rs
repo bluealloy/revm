d2c871a0ec91f8ce8cee79b834bb1c4f813ab0b6
Eric Woolsey <ewoolsey@ualberta.ca>
2025-12-03 15:26:36 -0800

    feat: DatabaseCommitExt::drain_balances

--- crates/database/interface/src/lib.rs
+++ crates/database/interface/src/lib.rs
@@ -196,22 +196,53 @@ pub trait DatabaseCommitExt: Database + DatabaseCommit {
         balances: impl IntoIterator<Item = (Address, u128)>,
     ) -> Result<(), Self::Error> {
         // Make transition and update cache state
-        let balances = balances.into_iter();
-        let mut transitions: HashMap<Address, Account> = HashMap::default();
-        transitions.reserve(balances.size_hint().0);
-        for (address, balance) in balances {
-            let mut original_account = match self.basic(address)? {
-                Some(acc_info) => Account::from(acc_info),
-                None => Account::new_not_existing(0),
-            };
-            original_account.info.balance = original_account
-                .info
-                .balance
-                .saturating_add(U256::from(balance));
-            original_account.mark_touch();
-            transitions.insert(address, original_account);
-        }
-        self.commit(transitions);
+        let transitions = balances
+            .into_iter()
+            .map(|(address, balance)| {
+                let mut original_account = match self.basic(address)? {
+                    Some(acc_info) => Account::from(acc_info),
+                    None => Account::new_not_existing(0),
+                };
+                original_account.info.balance = original_account
+                    .info
+                    .balance
+                    .saturating_add(U256::from(balance));
+                original_account.mark_touch();
+                Ok((address, original_account))
+            })
+            // Unfortunately must collect here to short circuit on error
+            .collect::<Result<Vec<_>, _>>()?;
+
+        self.commit_iter(transitions);
         Ok(())
+    }
+
+    /// Drains balances from given account and return those values.
+    ///
+    /// It is used for DAO hardfork state change to move values from given accounts.
+    fn drain_balances(
+        &mut self,
+        addresses: impl IntoIterator<Item = Address>,
+    ) -> Result<Vec<u128>, Self::Error> {
+        // Make transition and update cache state
+        let addresses_iter = addresses.into_iter();
+        let (lower, _) = addresses_iter.size_hint();
+        let mut transitions = Vec::with_capacity(lower);
+        let balances = addresses_iter
+            .map(|address| {
+                let mut original_account = match self.basic(address)? {
+                    Some(acc_info) => Account::from(acc_info),
+                    None => Account::new_not_existing(0),
+                };
+                let balance = original_account.info.balance;
+                original_account.info.balance = U256::ZERO;
+                original_account.mark_touch();
+                transitions.push((address, original_account));
+                Ok(balance.try_into().unwrap())
+            })
+            .collect::<Result<Vec<_>, _>>()?;
+
+        self.commit_iter(transitions);
+        Ok(balances)
     }
 }
