4b48adecad5b3724ddf57ec31a22ec49bc9c0f6e
Haythem Sellami <17862704+haythemsellami@users.noreply.github.com>
2025-12-16 16:22:46 +0700

    missing exports

--- crates/monad-revm/src/api/mod.rs
+++ crates/monad-revm/src/api/mod.rs
@@ -2,6 +2,6 @@ pub mod builder;
 pub mod default_ctx;
 pub mod exec;
 
-pub use builder::MonadBuilder;
-pub use default_ctx::DefaultMonad;
+pub use builder::{DefaultMonadEvm, MonadBuilder};
+pub use default_ctx::{DefaultMonad, MonadContext};
 pub use exec::{MonadContextTr, MonadError};
