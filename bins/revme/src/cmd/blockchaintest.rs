80611ea00ab6f96a4cadd534ece5407d4a54f508
Ryan <RyanBGG@proton.me>
2025-12-23 10:31:20 +0800

    sort storage keys and print all slots in blockchaintest output

--- bins/revme/src/cmd/blockchaintest.rs
+++ bins/revme/src/cmd/blockchaintest.rs
@@ -87,14 +87,12 @@ pub fn find_all_json_tests(path: &Path) -> Vec<PathBuf> {
     if path.is_file() {
         vec![path.to_path_buf()]
     } else {
-        let mut files: Vec<_> = WalkDir::new(path)
+        WalkDir::new(path)
             .into_iter()
             .filter_map(Result::ok)
             .filter(|e| e.path().extension() == Some("json".as_ref()))
             .map(DirEntry::into_path)
-            .collect();
-        files.sort();
-        files
+            .collect()
     }
 }
 
