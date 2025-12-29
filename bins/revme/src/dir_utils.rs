b4b90da3678fd8591d1e51d8714dca6d4fd63740
iPLAY888 <133153661+letmehateu@users.noreply.github.com>
2025-12-29 23:16:03 +0300

    Update dir_utils.rs

--- bins/revme/src/dir_utils.rs
+++ bins/revme/src/dir_utils.rs
@@ -1,16 +1,18 @@
 use std::path::{Path, PathBuf};
 use walkdir::{DirEntry, WalkDir};
 
+/// Find all JSON test files in the given path.
+/// If path is a file, returns it in a vector.
+/// If path is a directory, recursively finds all .json files.
 pub fn find_all_json_tests(path: &Path) -> Vec<PathBuf> {
-    WalkDir::new(path)
-        .into_iter()
-        .filter_map(|e| e.ok())
-        .filter(|e| {
-            e.path()
-                .extension()
-                .map(|ext| ext == "json")
-                .unwrap_or(false)
-        })
-        .map(DirEntry::into_path)
-        .collect::<Vec<PathBuf>>()
+    if path.is_file() {
+        vec![path.to_path_buf()]
+    } else {
+        WalkDir::new(path)
+            .into_iter()
+            .filter_map(Result::ok)
+            .filter(|e| e.path().extension() == Some("json".as_ref()))
+            .map(DirEntry::into_path)
+            .collect()
+    }
 }
