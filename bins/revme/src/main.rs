d0f229c6bcf8e2b77c36e3cdd06ffbb62ad70e99
Ryan <RyanBGG@proton.me>
2026-01-07 01:35:44 +0800

    feat: improve error reporting with color-eyre

--- bins/revme/src/main.rs
+++ bins/revme/src/main.rs
@@ -1,17 +1,15 @@
 use clap::Parser;
+use color_eyre::eyre::Result;
 use revme::cmd::MainCmd;
-use std::process::ExitCode;
 
-fn main() -> ExitCode {
+fn main() -> Result<()> {
+    color_eyre::install()?;
+    
     if std::env::var_os("RUST_BACKTRACE").is_none() {
         unsafe { std::env::set_var("RUST_BACKTRACE", "1") };
     }
 
-    match MainCmd::parse().run() {
-        Ok(()) => ExitCode::SUCCESS,
-        Err(e) => {
-            eprintln!("error:\n- {e}\n- {e:#?}");
-            ExitCode::FAILURE
-        }
-    }
+    MainCmd::parse().run()?;
+    
+    Ok(())
 }
