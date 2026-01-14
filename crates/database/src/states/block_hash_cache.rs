20fec1e824d0446783d8aa77981fb867cf2f6e47
malik672 <aremumalik05@gmail.com>
2026-01-14 20:53:59 +0100

    bump

--- crates/database/src/states/block_hash_cache.rs
+++ crates/database/src/states/block_hash_cache.rs
@@ -1,13 +1,9 @@
 use primitives::{alloy_primitives::B256, BLOCK_HASH_HISTORY};
 
 const BLOCK_HASH_HISTORY_USIZE: usize = BLOCK_HASH_HISTORY as usize;
-const BLOCK_HASH_HISTORY_MINUS_ONE: u64 = BLOCK_HASH_HISTORY - 1;
-
 /// A fixed-size cache for the 256 most recent block hashes.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct BlockHashCache {
-    /// The block number corresponding to index 0 of the `hashes` array.
-    start_block: u64,
     /// A fixed-size array holding the block hashes.
     /// Since we only store the most recent 256 block hashes, this array has a length of 256.
     /// The reason we store block number alongside its hash is to handle the case where it wraps around, so we can verify the block number.
@@ -21,11 +17,10 @@ impl Default for BlockHashCache {
 }
 
 impl BlockHashCache {
-    /// Creates a new empty BlockHashCache.
+    /// Creates a new empty BlockHashCache of length [BLOCK_HASH_HISTORY].
     #[inline]
     pub const fn new() -> Self {
         Self {
-            start_block: 0,
             hashes: [(0, B256::ZERO); BLOCK_HASH_HISTORY_USIZE],
         }
     }
@@ -35,11 +30,6 @@ impl BlockHashCache {
     pub const fn insert(&mut self, block_number: u64, block_hash: B256) {
         let index = (block_number % BLOCK_HASH_HISTORY) as usize;
         self.hashes[index] = (block_number, block_hash);
-        if block_number >= self.start_block + BLOCK_HASH_HISTORY {
-            // this only runs when block_number >= self.start_block + 256
-            // Overflow impossible due to the check above
-            self.start_block = block_number - BLOCK_HASH_HISTORY_MINUS_ONE;
-        }
     }
 
     /// Retrieves the block hash for the given block number, if it exists in the cache.
