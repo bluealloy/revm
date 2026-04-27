12a781597693de8e6885ba68eb76e7e57578e2d5
rakita <dragan0rakita@gmail.com>
2026-04-27 09:51:42 +0200

    fix: enable state refund on tx crate

--- crates/handler/src/precompile_provider.rs
+++ crates/handler/src/precompile_provider.rs
@@ -97,6 +97,7 @@ pub fn precompile_output_to_interpreter_result(
 
     // set state gas, reservoir is already set in the Gas constructor
     result.gas.set_state_gas_spent(output.state_gas_used as i64);
+    result.gas.set_refill_amount(output.refill_amount);
     result.gas.record_refund(output.gas_refunded);
 
     // spend used gas.
